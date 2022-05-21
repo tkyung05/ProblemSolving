daa94903c1c1d0df2bd969fe1a6ca30221885065
tkyung05 <tkyung6761@gmail.com>
2022-05-21 15:07:43 +0900

    TIS :: BOJ 2869 달팽이는 올라가고 싶다 문제 성공.

--- codeTest.py
+++ codeTest.py
@@ -1,4 +1,13 @@
-T = int(input())
+a, b, v = map(int, input().split())
 
-for i in range(T) :
-    H, W, N = map(int, input())
+one_day = a - b
+v -= a
+count = 1
+
+count += v // one_day
+
+if v % one_day > 0 :
+    count += 1
+
+print(count)
+
