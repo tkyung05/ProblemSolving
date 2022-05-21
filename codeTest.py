842bd646edc0c1fbdf4d2a0f2aa2d1ac3a651cd3
tkyung05 <tkyung6761@gmail.com>
2022-05-21 15:01:04 +0900

    TIS :: BOJ 2869 달팽이는 올라가고 싶다 문제 성공.

--- codeTest.py
+++ codeTest.py
@@ -1,14 +1,4 @@
-n = int(input())
+T = int(input())
 
-honeyComb = 6
-count = 1
-result = 1
-
-while True :
-    if result >= n :
-        break
-
-    result += honeyComb * count
-    count += 1
-
-print(count)
+for i in range(T) :
+    H, W, N = map(int, input())
