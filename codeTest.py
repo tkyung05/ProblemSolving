a8b031e26dce284b4812c9ceb298a95ef4388be8
tkyung05 <tkyung6761@gmail.com>
2022-05-23 20:03:19 +0900

    TIS :: BOJ 1929 소수 문제 반성공.(로직은 맞지만 시간 초과로 다른사람 소스봤음).

--- codeTest.py
+++ codeTest.py
@@ -1,48 +1,29 @@
-m = int(input())
-n = int(input())
+m, n = map(int, input().split())
 
-data = []
-primeCh = True
-sum = 0
-min = 0
-
-def FindByPrime(num) :
-   global sum
-   global primeCh
-   global data
-
-   if num == 1 :
-      return
-
-   bug = False
-
-   for i in range(2, num) :
-      if num % i == 0 :
-         bug = True
-   if not bug:
-      data.append(num)
-      sum += num
-      primeCh = False
-
-
-# main
 for num in range(m, n + 1) :
-   if num != 2 :
-      FindByPrime(num)
-   else :
-      data.append(num)
-      sum += num
-      primeCh = False
+
+    if num == 1 :
+        continue
+
+    for i in range(2, int(num ** 0.5) + 1) :
+        if num % i == 0 :
+            break
+    else :
+        print(num)
 
 
 
 
-if primeCh :
-   data.append(-1)
 
-data.sort()
-min = data[0]
 
-if not primeCh :
-   print(sum)
-print(min)
+
+
+
+
+
+
+
+
+
+
+
