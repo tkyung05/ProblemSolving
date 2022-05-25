0748390649e3cf3d4f32c285f68114f2cb6d577b
tkyung05 <tkyung6761@gmail.com>
2022-05-25 17:11:37 +0900

    TIS :: BOJ 10872 팩토리얼 성공.

--- codeTest.py
+++ codeTest.py
@@ -1,33 +1,9 @@
-import sys
+n = int(input())
 
-limit = 123456
+def fac(n) :
+    if n == 1 or n == 0 :
+        return 1
+    return n * fac(n - 1)
 
-eratos_data = [True] * (2 * limit + 1)
-eratos_data[0], eratos_data[1] = False, False
-
-for i in range(2, int(len(eratos_data) ** 0.5)) :
-    if eratos_data[i] :
-        for j in range(i + i, len(eratos_data), i) :
-            eratos_data[j] = False
-
-
-while True :
-    n = int(sys.stdin.readline())
-    count = 0
-    if n == 0 :
-        break
-
-    if n == 1 :
-        print(1)
-        continue
-
-    for i in range(n + 1, 2 * n) :
-        if eratos_data[i] :
-            count += 1
-
-    print(count)
-
-
-
-
+print(fac(n))
 
