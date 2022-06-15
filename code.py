ef9e08de8ce05ad51bb2b729a2f4901cf39f9368
tkyung05 <tkyung6761@gmail.com>
2022-06-16 08:39:35 +0900

    BOJ 9020 골드바흐의 추측 성공!

--- code.py
+++ code.py
@@ -1,14 +1,27 @@
 import sys
 T = int(input())
 
+limit = 10000
+
+eratos_data = [True] * (limit + 1)
+eratos_data[0], eratos_data[1] = False, False
+
+for i in range(2, int(len(eratos_data) ** 0.5)) :
+    if eratos_data[i] :
+        for j in range(i + i, len(eratos_data), i) :
+            eratos_data[j] = False
+
+
 for _ in range(T):
-    K = int(sys.stdin.readline())
-    N = int(sys.stdin.readline()) 
-    F = [n for n in range(1, N + 1)]
+    n = int(sys.stdin.readline())
+    min = 10000
+    a, b = 0, 0
+    r = 0
 
-    for _ in range(K):
-        for i in range(1, N):
-            F[i] += F[i - 1]
-    print(F[-1])
-
-    
+    for i in range(n // 2, n):
+        r = n - i
+        if eratos_data[i] and eratos_data[r]:
+            if min > i - r:
+                min = i - r
+                a, b = r, i
+    print (f'{a} {b}')
