98a9d84c163b341d0f4a0dbc809fdb372e5d02cc
tkyung05 <tkyung6761@gmail.com>
2022-06-18 23:31:24 +0900

    BOJ 2798 블랙잭

--- code.py
+++ code.py
@@ -1,22 +1,13 @@
-def draw_star(n, data):
-    new_data = []
-    if n == 3:
-        return data
-    else:
-        for i in data:
-            new_data.append(i * 3)
-        for i in data:
-            new_data.append(i + ' ' * len(data) + i)
-        for i in data:
-            new_data.append(i * 3)
-        
-        return draw_star(n//3, new_data)
-    
+n, m = map(int, input().split())
+data = list(map(int, input().split()))
+result = 0
 
-n = int(input())
-default_data = ['***', '* *', '***']
-result_data = draw_star(n, default_data)
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            if data[i] + data[j] + data[k] > m:
+                continue
+            else:
+                result = max(result, data[i] + data[j] + data[k])
 
-for i in result_data:
-    print(i)
-        
+print(result)
