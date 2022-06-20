205bcfa8072492562617a4ee29f8fc1e18133cd5
tkyung05 <tkyung6761@gmail.com>
2022-06-20 11:28:34 +0900

    BOJ 10989 수 정렬하기 3

--- code.py
+++ code.py
@@ -1,20 +1,12 @@
 import sys
+n = int(input())
+data = [0] * 10001
 
-T = int(input())
-x_data = []
-y_data = []
-rank_data = []
+for _ in range(n):
+    data[int(sys.stdin.readline())] += 1 
 
-for _ in range(T):
-    x, y = map(int, sys.stdin.readline().split())
-    x_data.append(x)
-    y_data.append(y)
+for i in range(len(data)):
+    if data[i] != 0:
+        for _ in range(data[i]):
+            print(i)
 
-for i in range(T):
-    count = 1
-    for j in range(T):
-        if x_data[j] > x_data[i] and y_data[j] > y_data[i]:
-            count += 1
-    rank_data.append(count)
-
-print(*rank_data)
