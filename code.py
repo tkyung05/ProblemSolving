617a3f28d53c03080d885010dcc50dcbb05652c2
tkyung05 <tkyung6761@gmail.com>
2022-06-21 17:56:28 +0900

    BOJ 1181 단어 정렬

--- code.py
+++ code.py
@@ -2,13 +2,16 @@ import sys
 
 n = int(input())
 data = []
+result_data = []
 
 for i in range(n):
-  x, y = map(int, sys.stdin.readline().split())
-  data.append([y, x])
-data.sort()
+  data.append(sys.stdin.readline())
+set_data = list(set(data))
 
-for i in range(n):
-  print(data[i][1], data[i][0])
+for i in set_data:
+  result_data.append((len(i), i))
+result_data.sort()
 
+for i in range(len(result_data)):
+  print(result_data[i][1], end='')
     
