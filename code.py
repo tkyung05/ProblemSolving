e66e43a4e5a5c497709139894379a2dbd260efeb
tkyung05 <tkyung6761@gmail.com>
2022-06-22 22:54:36 +0900

    BOJ 9093 단어 뒤집기

--- code.py
+++ code.py
@@ -1,44 +1,10 @@
 import sys
-n = int(input())
+T = int(input())
 
-i = 0
-stack = []
-
-def push(num):
-    stack.append(int(num))
-    global i
-    i += 1
-
-def pop():
-    global i
-    if len(stack) != 0:
-        print(stack.pop(i - 1))
-        i -= 1
-    else:
-        print(-1)
-
-def empty():
-    if len(stack) == 0:
-        print(1)
-    else:
-        print(0)
-
-def top():
-    if len(stack) != 0:
-        print(stack[i - 1])
-    else:
-        print(-1)
-
-for _ in range(n):
-    cmd = list(sys.stdin.readline().split())
-
-    if cmd[0] == "push":
-        push(cmd[1])
-    if cmd[0] == "pop":
-        pop()
-    if cmd[0] == "size":
-        print(len(stack))
-    if cmd[0] == "empty":
-        empty()
-    if cmd[0] == "top":
-        top()
+for i in range(T):
+    data = list(sys.stdin.readline().split())
+    for j in range(len(data)):
+        if j == int(len(data) - 1):
+            print(data[j][::-1])
+        else :
+            print(data[j][::-1], end=' ')
