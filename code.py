91b5d8644be0fde5d208b356e4c47523a4939970
tkyung05 <tkyung6761@gmail.com>
2022-06-22 19:41:48 +0900

    BOJ 10828 스택

--- code.py
+++ code.py
@@ -1,12 +1,44 @@
+import sys
 n = int(input())
-card_data = list(map(int, input().split()))
-m = int(input())
-get_data = list(map(int, input().split()))
 
-result_dict = {card_data[i] : 1 for i in range(len(card_data))}
+i = 0
+stack = []
 
-for i in range(len(get_data)):
-    if get_data[i] in result_dict:
-        print(1, end=' ')
+def push(num):
+    stack.append(int(num))
+    global i
+    i += 1
+
+def pop():
+    global i
+    if len(stack) != 0:
+        print(stack.pop(i - 1))
+        i -= 1
     else:
-        print(0, end=' ')
+        print(-1)
+
+def empty():
+    if len(stack) == 0:
+        print(1)
+    else:
+        print(0)
+
+def top():
+    if len(stack) != 0:
+        print(stack[i - 1])
+    else:
+        print(-1)
+
+for _ in range(n):
+    cmd = list(sys.stdin.readline().split())
+
+    if cmd[0] == "push":
+        push(cmd[1])
+    if cmd[0] == "pop":
+        pop()
+    if cmd[0] == "size":
+        print(len(stack))
+    if cmd[0] == "empty":
+        empty()
+    if cmd[0] == "top":
+        top()
