02f3dcd370d4ad5b9b685ec87ee3b7546f3315cd
tkyung05 <tkyung6761@gmail.com>
2022-06-22 17:20:13 +0900

    BOJ 1085 직사각형에서 탈출

--- code.py
+++ code.py
@@ -1,9 +1,3 @@
-n = int(input())
-num_data = list(map(int, input().split()))
+x, y, w, h = map(int, input().split())
 
-set_data = sorted(set(num_data))
-dic_data = {set_data[i] : i for i in range(len(set_data))}
-
-for i in num_data:
-  print(dic_data[i], end=' ')
-  
+print(min(x, y, w-x, h-y))
