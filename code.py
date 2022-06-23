21bfaa4c6f656fbe93c1c05a30744723ef678e93
tkyung05 <tkyung6761@gmail.com>
2022-06-23 12:28:08 +0900

    BOJ 4949 균형잡힌 세상

--- code.py
+++ code.py
@@ -1,15 +1,52 @@
 import sys
-T = int(input())
 
-stack = []
-for i in range(T):
-    num = int(sys.stdin.readline())
-    if num == 0:
-        stack.pop() 
+def str_check(data):
+    for i in range(len(list(data))):
+        # 소괄호 조건
+        if data[i] == '(':
+            stack.append('(')
+
+        elif data[i] == ')':
+            if len(list(stack)) == 0:
+                return 'no'
+            elif len(list(stack)) != 0:
+                if stack[-1] == '[':
+                    return 'no'
+                else:
+                    stack.pop()
+                
+
+        # 대괄호 조건 
+        elif data[i] == '[':
+            stack.append('[')
+
+        elif data[i] == ']':
+            if len(list(stack)) == 0:
+                return 'no'
+            elif len(list(stack)) != 0:
+                if stack[-1] == '(':
+                    return 'no'
+                else:
+                    stack.pop()
+
+        # 기타 문자열 넘기기
+        else:
+            continue
+
+    if len(list(stack)) == 0:
+        return 'yes'
     else:
-        stack.append(num)
+        return 'no'
+    
+    
 
-print(sum(stack))
 
-        
+while True:
+    data = sys.stdin.readline()
+    if data[0] == '.' and data[1] == '\n':
+        break 
+
+    stack = []
+    print(str_check(data))
+    
     
