daf75bac0273b3fed984a430d57d242a34268a0e
tkyung05 <tkyung6761@gmail.com>
2022-06-23 09:02:00 +0900

    BOJ 9012 괄호

--- code.py
+++ code.py
@@ -2,9 +2,18 @@ import sys
 T = int(input())
 
 for i in range(T):
-    data = list(sys.stdin.readline().split())
-    for j in range(len(data)):
-        if j == int(len(data) - 1):
-            print(data[j][::-1])
-        else :
-            print(data[j][::-1], end=' ')
+    data = list(sys.stdin.readline())
+    sum = 0
+    for j in data:
+        if j == "(":
+            sum += 1
+        if j == ")":
+            sum -= 1
+            if sum < 0:
+                print("NO")
+                break
+        
+    if sum == 0:
+        print("YES")
+    elif sum > 0:
+        print("NO")
