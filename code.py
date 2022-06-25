9ba94bd7276d4049234f08419ef513b72f5b0f5c
tkyung05 <tkyung6761@gmail.com>
2022-06-25 21:23:35 +0900

    BOJ 25305 커트라인 : 자존감 올리기용 문제 ㅎㅎ

--- code.py
+++ code.py
@@ -1,15 +1,7 @@
 import sys
-total = int(sys.stdin.readline())
-n = int(sys.stdin.readline())
+n, k = map(int, sys.stdin.readline().split())
+data = list(map(int, sys.stdin.readline().split()))
 
-sum_data = [0] * n 
+data.sort(reverse=True)
 
-for i in range(n):
-    x, y = map(int, sys.stdin.readline().split())
-    sum_data[i] = x * y
-
-if total == sum(sum_data):
-    print('Yes')
-else:
-    print('No')
-
+print(data[k - 1])
