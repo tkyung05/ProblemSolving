91be1f9e176c62763433906ac5b2950e6645621c
tkyung05 <tkyung6761@gmail.com>
2022-06-26 21:09:48 +0900

    BOJ 2230 수 고르기 : 투 포인터가 꼭 양쪽이 아니라 앞으로 같이 나아갈 수도 있다는 것을 알게 해준 문제

--- code.py
+++ code.py
@@ -1,24 +1,26 @@
 import sys
-
-n = int(input())
-sequence_data = list(map(int, sys.stdin.readline().split()))
-x = int(input())
-
-count = 0
-sequence_data.sort()
+n, m = map(int, sys.stdin.readline().split())
+data = [0] * n
+for i in range(n):
+    data[i] = int(sys.stdin.readline())
+data.sort()
 
 left = 0
-right = n - 1
-while left < right:
-    sum = sequence_data[left] + sequence_data[right]
-    if sum == x:
-        count += 1
-        left += 1
-        right -= 1
-    
-    if sum < x:
-        left += 1
-    if sum > x:
-        right -= 1
+right = 1
+most_min = 2000000000
 
-print(count)
+while left < n and  right < n:
+    sub = data[right] - data[left]
+
+    if most_min > sub and m <= sub:
+        most_min = sub
+        if most_min == m:
+            break
+        left += 1
+    
+    elif sub > m:
+        left += 1
+    elif sub < m:
+        right += 1
+
+print(most_min)
