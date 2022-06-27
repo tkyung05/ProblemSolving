03cc1e66da268ce7797f0b8584d7a569bce95b4c
tkyung05 <tkyung6761@gmail.com>
2022-06-27 13:52:15 +0900

    BOJ 11047 동전0

--- code.py
+++ code.py
@@ -1,30 +1,18 @@
 import sys
-n = int(sys.stdin.readline())
-queue = []
+n, k = map(int, sys.stdin.readline().split())
+data = [0] * n
+
 for i in range(n):
-    cmd = list(sys.stdin.readline().split())
-    if cmd[0] == 'push':
-        queue.append(int(cmd[1]))
-    elif cmd[0] == 'pop':
-        if len(queue) != 0:
-            print(queue.pop(0))
-        else:
-            print(-1)
-    elif cmd[0] == 'size':
-        print(len(queue))
-    elif cmd[0] == 'empty':
-        if len(queue) == 0:
-            print(1)
-        else:
-            print(0)
-    elif cmd[0] == 'front':
-        if len(queue) != 0:
-            print(queue[0])
-        else:
-            print(-1)
-    elif cmd[0] == 'back':
-        if len(queue) != 0:
-            print(queue[-1])
-        else:
-            print(-1)
-    
+    data[i] = int(sys.stdin.readline())
+data.sort(reverse=True)
+
+count = 0
+
+for i in range(n):
+    if k == 0:
+        break
+    if data[i] <= k:
+        count += (k // data[i])
+        k %= data[i]
+         
+print(count)
