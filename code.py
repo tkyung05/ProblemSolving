87b86bceb60d8f4bb1e9b4fd022ead595936e399
tkyung05 <tkyung6761@gmail.com>
2022-06-27 14:33:54 +0900

    BOJ 11399 ATM

--- code.py
+++ code.py
@@ -1,18 +1,11 @@
 import sys
-n, k = map(int, sys.stdin.readline().split())
-data = [0] * n
+n = int(sys.stdin.readline())
+data = list(map(int, sys.stdin.readline().split()))
+data.sort()
 
+result = 0
 for i in range(n):
-    data[i] = int(sys.stdin.readline())
-data.sort(reverse=True)
-
-count = 0
-
-for i in range(n):
-    if k == 0:
-        break
-    if data[i] <= k:
-        count += (k // data[i])
-        k %= data[i]
-         
-print(count)
+    result += sum(data)
+    data.pop()
+    
+print(result)
