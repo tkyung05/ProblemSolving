f1b6252080f6bc5635350d522b0dc811013a152b
tkyung05 <tkyung6761@gmail.com>
2022-06-28 12:28:22 +0900

    BOJ 1541 잃어버린 괄호 : 아이디어만 가져오고 혼자 해결함.

--- code.py
+++ code.py
@@ -1,24 +1,13 @@
 import sys
-n = int(sys.stdin.readline())
-a = 60 * 5
-b = 60
-c = 10
+data = list(sys.stdin.readline().split('-'))
 
-a_count, b_count, c_count = 0, 0, 0
+result = []
+for i in data:
+    temp = list(map(int, i.split('+')))
+    result.append(sum(temp))
 
-if n >= a:
-    a_count += n // a
-    n %= a
+ans = result[0]
+for i in range(1, len(result)):
+    ans -= result[i]
 
-if n >= b:
-    b_count += n // b
-    n %= b
-
-if n >= c:
-    c_count += n // c
-    n %= c
-
-if n == 0:
-    print(a_count, b_count, c_count)
-else:
-    print(-1)
+print(ans)
