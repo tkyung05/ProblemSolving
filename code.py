0b48a50feacffdf5c6981f964aa3bb3bad5bff71
tkyung05 <tkyung6761@gmail.com>
2022-06-28 22:33:35 +0900

    BOJ 2012 등수 매기기 : 너무 쉬워서 걱정..

--- code.py
+++ code.py
@@ -1,12 +1,12 @@
+from random import randrange
 import sys
 n = int(sys.stdin.readline())
 data = [0] * n
 for i in range(n):
     data[i] = int(sys.stdin.readline())
-data.sort(reverse=True)
+data.sort()
 
-sub = 0
-for i in range(2, n, 3):
-    sub += data[i]
-
-print(sum(data) - sub)
+result = [0] * n
+for i in range(n):
+    result[i] = abs(data[i] - (i+1))
+print(sum(result))
