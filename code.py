f9d9905284727df3126695ec735644117e982c5f
tkyung05 <tkyung6761@gmail.com>
2022-06-30 08:40:20 +0900

    BOJ 9237 이장님 초대 : 뇌 풀기 문제 실버5

--- code.py
+++ code.py
@@ -1,15 +1,3 @@
-from random import randrange
 import sys
-import heapq
 
-n = int(sys.stdin.readline())
-data = []
-for i in range(n):
-    num = (int(sys.stdin.readline()) * -1)
-    if num * -1 > 0:
-        heapq.heappush(data, num)
-    else:
-        if len(data) == 0:
-            print(0)
-        else:
-            print(heapq.heappop(data) * -1)
+
