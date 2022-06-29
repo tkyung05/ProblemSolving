6ea2d8573a34854180c1507852552dc24e56a787
tkyung05 <tkyung6761@gmail.com>
2022-06-29 12:35:34 +0900

    BOJ 1744 수 묶기 : 와 ㅜㅜ 내 힘으로 골드4 풀었다!! 스파게티 코드만 좀 고쳐보자 !

--- code.py
+++ code.py
@@ -1,22 +1,45 @@
 import sys
-n, m = map(int, sys.stdin.readline().split())
-bundle = [0] * m
-indiv = [0] * m
-for i in range(m):
-    bun, ind = map(int, sys.stdin.readline().split())
-    bundle[i] = bun
-    indiv[i] = ind
-bundle.sort()
-indiv.sort()
+n = int(sys.stdin.readline())
 
-result = [0] * 3
-remain = n % 6
-share = n // 6
+ordinary_data = []
+zero_data = []
+minus_data = []
 
-result[0] = bundle[0] * share + indiv[0] * remain
-result[1] = bundle[0] * share
-if remain > 0:
-    result[1] = bundle[0] * (share + 1)
-result[2] = indiv[0] * n
+for i in range(n):
+    num = int(sys.stdin.readline())
+    if num == 0:
+        zero_data.append(num)
+    elif num < 0:
+        minus_data.append(num)
+    elif num > 0:
+        ordinary_data.append(num)
 
-print(min(result))
+minus_data.sort()
+ordinary_data.sort(reverse=True)
+
+# kim_tk. 스파게티 코드 씹팔 ㅋ 
+
+# 자연수 데이터를 전부 묶거나 더해줌
+result = 0
+for i in range(0, len(ordinary_data), 2):
+    if i == len(ordinary_data) - 1:
+        result += ordinary_data[i]
+        break
+
+    if ordinary_data[i] == 1 or ordinary_data[i + 1] == 1:
+        result += ordinary_data[i] + ordinary_data[i + 1]    
+        continue  
+
+    result += ordinary_data[i] * ordinary_data[i + 1]
+
+
+# 음수 데이터를 전부 묶거나 더해줌
+if len(minus_data) != 0:
+    for i in range(0, len(minus_data), 2):
+        if i == len(minus_data) - 1:
+            if len(zero_data) == 0:
+                result += minus_data[i]
+            break
+        result += minus_data[i] * minus_data[i + 1]
+
+print(result)
