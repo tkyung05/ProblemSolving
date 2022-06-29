ba0a5e8a4c0991a49dc2e02dae419552c789e4d5
tkyung05 <tkyung6761@gmail.com>
2022-06-29 20:53:09 +0900

    BOJ 11000 강의실 배정 : heapq 모듈의 중요성 시간 복잡도 줄여줌

--- code.py
+++ code.py
@@ -1,45 +1,24 @@
 import sys
+import heapq
+
 n = int(sys.stdin.readline())
-
-ordinary_data = []
-zero_data = []
-minus_data = []
-
+data = []
 for i in range(n):
-    num = int(sys.stdin.readline())
-    if num == 0:
-        zero_data.append(num)
-    elif num < 0:
-        minus_data.append(num)
-    elif num > 0:
-        ordinary_data.append(num)
+    a, b = map(int, sys.stdin.readline().split())
+    data.append([a, b])
+data.sort()
 
-minus_data.sort()
-ordinary_data.sort(reverse=True)
+end_time = []
+heapq.heappush(end_time, data[0][1])
 
-# kim_tk. 스파게티 코드 씹팔 ㅋ 
-
-# 자연수 데이터를 전부 묶거나 더해줌
-result = 0
-for i in range(0, len(ordinary_data), 2):
-    if i == len(ordinary_data) - 1:
-        result += ordinary_data[i]
-        break
-
-    if ordinary_data[i] == 1 or ordinary_data[i + 1] == 1:
-        result += ordinary_data[i] + ordinary_data[i + 1]    
-        continue  
-
-    result += ordinary_data[i] * ordinary_data[i + 1]
-
-
-# 음수 데이터를 전부 묶거나 더해줌
-if len(minus_data) != 0:
-    for i in range(0, len(minus_data), 2):
-        if i == len(minus_data) - 1:
-            if len(zero_data) == 0:
-                result += minus_data[i]
-            break
-        result += minus_data[i] * minus_data[i + 1]
-
-print(result)
+for i in range(1, n):
+    if data[i][0] < end_time[0]:
+        heapq.heappush(end_time, data[i][1])
+    else:
+        heapq.heappop(end_time)
+        heapq.heappush(end_time, data[i][1])
+    
+print(len(end_time))
+            
+# blog 정리 해야겠다. 파이썬 우선순위 큐 heapq 모듈 사용법
+# + 사용 이유  
