5c997c5bd1e540b8df8d6db859f5cb19a0691b5c
tkyung05 <tkyung6761@gmail.com>
2022-06-29 21:58:51 +0900

    BOJ 1927 최소 힙

--- code.py
+++ code.py
@@ -1,24 +1,15 @@
+from random import randrange
 import sys
 import heapq
 
 n = int(sys.stdin.readline())
 data = []
 for i in range(n):
-    a, b = map(int, sys.stdin.readline().split())
-    data.append([a, b])
-data.sort()
-
-end_time = []
-heapq.heappush(end_time, data[0][1])
-
-for i in range(1, n):
-    if data[i][0] < end_time[0]:
-        heapq.heappush(end_time, data[i][1])
+    num = int(sys.stdin.readline())
+    if num > 0:
+        heapq.heappush(data, num)
     else:
-        heapq.heappop(end_time)
-        heapq.heappush(end_time, data[i][1])
-    
-print(len(end_time))
-            
-# blog 정리 해야겠다. 파이썬 우선순위 큐 heapq 모듈 사용법
-# + 사용 이유  
+        if len(data) == 0:
+            print(0)
+        else:
+            print(heapq.heappop(data))
