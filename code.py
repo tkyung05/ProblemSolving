379c9b7ceb39ca0ecbdb3695790fb5eb8a0d6158
tkyung05 <tkyung6761@gmail.com>
2022-06-30 22:05:59 +0900

    BOJ 4963 섬의 개수 : 처음으로 푼 그래프 탐색 문제! 로직은 ㄹㅇ 별로 안 어려운데 입력 받는게 너무 헷갈려서 진짜.. ㅜㅜ

--- code.py
+++ code.py
@@ -1,31 +1,43 @@
 import sys
-n, m = map(int, sys.stdin.readline().split())
 
-graph = []
-for i in range(n):
-    graph.append(list(map(int, sys.stdin.readline())))
-
-def bfs(x, y):
-    if x < 0 or x >= n or y < 0 or y >= m:
+def check(x, y):
+    if x < 0 or x >= h or y < 0 or y >= w:
         return False
-    if graph[x, y] == 0:
-        graph[x, y] = 1 # 방문 처리
+
+    if graph[x][y] == 1:
+        graph[x][y] = 0 
 
         # 재귀로 주변 상하좌우 모두 호출하여 방문처리
-        bfs(x - 1, y) 
-        bfs(x + 1, y)
-        bfs(x, y - 1)
-        bfs(x, y + 1)
+        check(x - 1, y) 
+        check(x + 1, y)
+        check(x, y - 1)
+        check(x, y + 1)
+        # 킹각선..
+        check(x - 1, y - 1) 
+        check(x - 1, y + 1)
+        check(x + 1, y - 1)
+        check(x + 1, y + 1)
 
         # 처음 0 발견한 한번만 True 리턴
         return True
-    # 1이라면 그냥 False 리턴 
+    # 1 이라면 그냥 False 리턴 
     return False
 
-result = 0
-for i in range(n):
-    for j in range(m):
-        if bfs(i, j) == True:
-            result += 1
 
-print(result)
+
+while True:
+    w, h = map(int, sys.stdin.readline().split())
+    
+    if w + h == 0:
+        break
+
+    graph = []
+    for _ in range(h):
+        graph.append(list(map(int, input().split())))
+
+    count = 0
+    for i in range(h):
+        for j in range(w):
+            if check(i, j) == True:
+                count += 1
+    print(count)
