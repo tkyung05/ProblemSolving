22b92240fc4ef29870caa072f7b0a97c5a1d78da
tkyung05 <tkyung6761@gmail.com>
2022-06-30 17:21:46 +0900

    BOJ 10866 덱 : 그냥 간단한 자료구조  구현 문제 자존감 올리기용 문제임 ㅎㅋ

--- code.py
+++ code.py
@@ -1,3 +1,46 @@
+from collections import deque
 import sys
 
+n = int(sys.stdin.readline())
+deque_data = deque()
+for i in range(n):
+    cmd = list(sys.stdin.readline().split())
+    
+    if cmd[0] == 'push_front':
+        deque_data.appendleft(int(cmd[1]))
+    
+    elif cmd[0] == 'push_back':
+        deque_data.append(int(cmd[1]))
+    
+    elif cmd[0] == 'pop_front':
+        if deque_data:
+            print(deque_data.popleft())
+        else:
+            print(-1)
+    
+    elif cmd[0] == 'pop_back':
+        if deque_data:
+            print(deque_data.pop())
+        else:
+            print(-1)
 
+    elif cmd[0] == 'size':
+        print(len(deque_data))
+    
+    elif cmd[0] == 'empty':
+        if deque_data:
+            print(0)
+        else:
+            print(1)
+    
+    elif cmd[0] == 'front':
+        if deque_data:
+            print(deque_data[0])
+        else:
+            print(-1)
+    
+    elif cmd[0] == 'back':
+        if deque_data:
+            print(deque_data[-1])
+        else:
+            print(-1)
