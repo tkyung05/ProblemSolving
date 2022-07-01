d85705f5d58826f006d588739fc2e31a0cb160f4
tkyung05 <tkyung6761@gmail.com>
2022-07-01 12:38:43 +0900

    BOJ 2667 단지번호붙이기 : 성공!! bfs 직접 짠 코드로 바로 성공

--- code.py
+++ code.py
@@ -1,33 +1,52 @@
 from collections import deque
 
-h, w = map(int, input().split())
+n = int(input())
 
-def bfs_check(x, y):
-    queue = deque()
-    queue.append((x, y))
+data = []
+def bfs_finder(x, y):
+    if graph[x][y] == 0 or graph[x][y] == 2:
+        return 0
+    
+    count = 1
 
-    while queue:
-        x, y = queue.popleft()
+    queue_data = deque()
+    queue_data.append((x, y))
+    graph[x][y] = 2
+    
+    while queue_data:
+        x, y = queue_data.popleft()
 
         for i in range(4):
             nx = x + dx[i]
-            ny = y + dy[i]
+            ny = y + dy[i]        
 
-            if nx < 0 or nx >= h or ny < 0 or ny >= w:
+            if nx < 0 or nx >= n or ny < 0 or ny >= n:
                 continue
-            if graph[nx][ny] != 1:
-                continue
-            
-            graph[nx][ny] = graph[x][y] + 1
-            queue.append((nx, ny))
-    return print(graph[h - 1][w - 1])
-        
+            if graph[nx][ny] == 1:
+                count += 1
+                graph[nx][ny] = 2
+                queue_data.append((nx, ny))
+
+    return count
+                    
+
 
 graph = []
-for _ in range(h):
+for _ in range(n):
     graph.append(list(map(int, input())))
-    
+
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-bfs_check(0, 0)
+
+for i in range(n):
+    for j in range(n):
+        num = bfs_finder(i, j)
+        if num != 0:
+            data.append(num)
+
+data.sort()
+print(len(data))
+
+for i in data:
+    print(i)
