cb332287d30a5d5a71a5d38c8c535f2a7ff6bdc2
tkyung05 <tkyung6761@gmail.com>
2022-07-02 00:30:10 +0900

    BOJ 1926 그림 : bfs 사용 하여 탐색 수행

--- code.py
+++ code.py
@@ -1,40 +1,54 @@
 import sys
+from collections import deque
 sys.setrecursionlimit(10**4)
 
-T = int(input())
+
+def bfs_finder(x, y):
+    if graph[x][y] == 0:
+        return 0
+    
+    queue = deque()
+    queue.append((x, y))
+    graph[x][y] = 0
+    
+    count = 1
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= h or ny < 0 or ny >= w:
+                continue
+            if graph[nx][ny] == 1:
+                count += 1
+                queue.append((nx, ny))
+                graph[nx][ny] = 0
+    
+    return count
+
+
+h, w = map(int, input().split())
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def dfs_finder(x, y):
-    if x < 0 or x >= h or y < 0 or y >= w:
-        return False
+graph = []
+for _ in range(h):
+    graph.append(list(map(int, input().split())))
 
-    if cab_graph[x][y] == 1:
-        cab_graph[x][y] = 0 # 방문 처리
-        
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            dfs_finder(nx, ny)
-        return True
-
-    return False
-        
-
-for _ in range(T):
-    w, h, k = map(int, input().split())
-    cab_graph = [[0 for _ in range(w)] for _ in range(h)]
-
-    for _ in range(k):
-        x, y = map(int, input().split())
-        cab_graph[y][x] = 1
-
-    count = 0
-    for i in range(h):
-        for j in range(w):
-            if dfs_finder(i, j):
-                count += 1
-    
-    print(count)
-    
+result = []
+for i in range(h):
+    for j in range(w):
+        num = bfs_finder(i, j)
+        if num != 0:
+            result.append(num)
+            
+if len(result) == 0:
+    print(0)
+    print(0)
+else:
+    result.sort(reverse=True)
+    print(len(result))
+    print(result[0])    
