d9315580c4adb9b18bad4b0936f11246c933df93
tkyung05 <tkyung6761@gmail.com>
2022-07-02 16:20:55 +0900

    BOJ 11724 연결 요소의 개수 : 1트만에 성공!기분 좋았음.. ㅎ bfs로 풀음

--- code.py
+++ code.py
@@ -1,35 +1,32 @@
 from collections import deque
 
-com = int(input())
-n_pair = int(input())
+v, e = map(int, input().split())
 
-graph = [[] for _ in range(com+1)]
-visited = [False] * (com + 1)
+graph = [[] for _ in range(v + 1)]
+visited = [False] * (v + 1)
 
-for _ in range(n_pair):
+for i in range(e):
     n1, n2 = map(int, input().split())
     graph[n1].append(n2)
     graph[n2].append(n1)
 
 
-def bfs_finder(start):
+def bfs(start):
     queue = deque()
     queue.append(start)
     visited[start] = True
-    
+
     while queue:
         cur_node = queue.popleft()
-
         for i in graph[cur_node]:
             if not visited[i]:
                 queue.append(i)
                 visited[i] = True
-
-bfs_finder(1)
-
+        
 result = 0
-for i in range(2, com+1):
-    if visited[i]: 
+for i in range(1, v + 1):
+    if not visited[i]:
+        bfs(i)
         result += 1
     
 print(result)
