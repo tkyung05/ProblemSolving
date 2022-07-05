5d2a84ea28acdba6b7890e3661fa7f115b71f5ce
tkyung05 <tkyung6761@gmail.com>
2022-07-05 22:02:04 +0900

    BOJ 14490 백대열 : 유클리드 호제법으로 최대공약수 쉽게 구하는 법 배움

--- code.py
+++ code.py
@@ -1,43 +1,13 @@
-from collections import deque
-import sys
+n, m = map(int, input().split(':'))
 
+def GCD(n, m):
+    while True:
+        tmp = n % m
+        if tmp == 0:
+            return m
+        n = m
+        m = tmp 
+        
+GCD_num = GCD(max(n, m), min(n, m))
 
-T = int(sys.stdin.readline())
-
-dx = [2, 2, 1, 1, -2, -2, -1, -1]
-dy = [1, -1, 2, -2, 1, -1, 2, -2]
-
-def bfs(x, y, dis_x, dis_y, n, visited):
-    queue = deque([(x, y)])
-    visited[x][y] = True
-    
-    while queue:
-        x, y = queue.popleft()
-        if x == dis_x and y == dis_y:
-            return graph[x][y]
-
-        for i in range(8):
-            nx, ny = x + dx[i], y + dy[i]
-
-            if nx < 0 or nx >= n or ny < 0 or ny >= n:
-                continue
-            if not visited[nx][ny]:
-                graph[nx][ny] = graph[x][y] + 1
-                visited[nx][ny] = True
-                queue.append((nx, ny))
-
-
-
-for _ in range(T):
-    n = int(sys.stdin.readline())
-    x, y = map(int, sys.stdin.readline().split())
-    dis_x, dis_y = map(int, sys.stdin.readline().split())
-
-    graph = [[0] * n for _ in range(n)]
-    visited = [[False] * n for _ in range(n)]
-
-    print(bfs(x, y, dis_x, dis_y, n, visited))
-    
-
-    
-    
+print(f'{n // GCD_num}:{m // GCD_num}')
