fc599002f7e83ddcabef03c2ac0ab9ad99d5ee4f
tkyung05 <tkyung6761@gmail.com>
2022-07-05 23:34:48 +0900

    BOJ 2644 촌수계산 : 크으... 또 활용! 해버렸다. 간이 메모이제이션 ㅋ

--- code.py
+++ code.py
@@ -1,32 +1,32 @@
 from collections import deque
+import sys
 
-n = int(input())
-leng = int(input())
+n = int(sys.stdin.readline())
+start, end = map(int, sys.stdin.readline().split())
+e = int(sys.stdin.readline())
 
 graph = [[] for _ in range(n + 1)]
-visited = [0] * (n + 1)
-
-for i in range(leng):
-    n1, n2 = map(int, input().split())
+for i in range(e):
+    n1, n2 = map(int, sys.stdin.readline().split())
     graph[n1].append(n2)
     graph[n2].append(n1)
 
+visited = [0] * (n + 1)
+
 def bfs():
-    queue = deque([1])
-    visited[1] = 1
-    count = 0
+    queue = deque([start])
 
     while queue:
         v = queue.popleft()
-
-        if visited[v] == 3:
-            return count
+        
+        if v == end:
+            return print(visited[v])
 
         for i in graph[v]:
             if not visited[i]:
-                count += 1
                 visited[i] = visited[v] + 1
                 queue.append(i)
-    return count
+    
+    return print(-1)
 
-print(bfs())
+bfs()
