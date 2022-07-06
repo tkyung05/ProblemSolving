4739bc65ba879ba5eafe3546a2a27b032fa6e683
tkyung05 <tkyung6761@gmail.com>
2022-07-06 09:06:33 +0900

    BOJ 7576 토마토 : 크 지렸다 원큐만에 솔브 성공! ... 1시간 좀 넘게 걸리긴 했는데.ㅋㅋ

--- code.py
+++ code.py
@@ -1,32 +1,53 @@
 from collections import deque
 import sys
 
-n = int(sys.stdin.readline())
-start, end = map(int, sys.stdin.readline().split())
-e = int(sys.stdin.readline())
+w, h = map(int, sys.stdin.readline().split())
+graph = []
+for _ in range(h):
+    graph.append(list(map(int, sys.stdin.readline().split())))
 
-graph = [[] for _ in range(n + 1)]
-for i in range(e):
-    n1, n2 = map(int, sys.stdin.readline().split())
-    graph[n1].append(n2)
-    graph[n2].append(n1)
+dy = [1, -1, 0, 0]
+dx = [0, 0, -1, 1]
 
-visited = [0] * (n + 1)
+
+visited = [[0] * w for _ in range(h)]
+tomato_node = deque()
+
+zero_check = False
+
+for i in range(h):
+    for j in range(w):
+        if graph[i][j] == 1:
+            tomato_node.append((i, j))
+            visited[i][j] = 1
+
+        elif graph[i][j] == -1:
+            visited[i][j] = -1
 
 def bfs():
-    queue = deque([start])
+    while tomato_node:
+        y, x = tomato_node.popleft()
 
-    while queue:
-        v = queue.popleft()
-        
-        if v == end:
-            return print(visited[v])
+        for i in range(4):
+            ny, nx = y + dy[i], x + dx[i]
 
-        for i in graph[v]:
-            if not visited[i]:
-                visited[i] = visited[v] + 1
-                queue.append(i)
-    
-    return print(-1)
-
+            if ny < 0 or ny >= h or nx < 0 or nx >= w:
+                continue
+            if visited[ny][nx] == 0:
+                visited[ny][nx] = visited[y][x] + 1
+                tomato_node.append((ny, nx))
 bfs()
+
+result = -10
+for i in range(h):
+    for j in range(w):
+        if visited[i][j] == 0:
+            result = 0
+            break
+        elif visited[i][j] > result:
+            result = visited[i][j]
+    
+    if result == 0:
+        break
+
+print(result - 1)
