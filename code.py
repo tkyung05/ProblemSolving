805ad8e6711f921a5fdfbd5079e5bd8b35c81339
tkyung05 <tkyung6761@gmail.com>
2022-07-06 16:51:55 +0900

    BOJ 16953 A -> B

--- code.py
+++ code.py
@@ -1,44 +1,28 @@
 from collections import deque
-import sys
 
-h, w, k = map(int, sys.stdin.readline().split())
+a, b = map(int, input().split())
 
-graph = [[0] * w for _ in range(h)]
-for _ in range(k):
-    y, x = map(int, sys.stdin.readline().split())
-    graph[y-1][x-1] = 1
+limit = 1000000001
+graph = {}
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, -1, 1]
+def bfs(a):
+    queue = deque([a])
+    graph[str(a)] = 1
 
-def bfs(y, x):
-    queue = deque([(y, x)])
-    graph[y][x] = 2
-    count = 1
-    
     while queue:
-        y, x = queue.popleft()
-        for i in range(4):
-            ny, nx = y + dy[i], x + dx[i]
+        n = queue.popleft()
+        one_n = int(str(n) + '1')
 
-            if ny < 0 or ny >= h or nx < 0 or nx >= w:
+        if n == b:
+            return print(graph[str(n)]) 
+
+        for i in (n*2, one_n):
+            if i < 0 or i >= limit:
                 continue
-            if graph[ny][nx] == 1:
-                count += 1
-                graph[ny][nx] = 2
-                queue.append((ny, nx))
-    
-    return count
+            if not str(i) in graph:
+                graph[str(i)] = graph[str(n)] + 1
+                queue.append(i)
 
-result = 0
-for i in range(h):
-    for j in range(w):
-        if graph[i][j] == 1:
-            num = bfs(i, j)
-            if result < num:
-                result = num
+    return print(-1)
 
-print(result)
-
-
-
+bfs(a)
