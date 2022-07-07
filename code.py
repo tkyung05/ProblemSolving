d9503debeca9d9b069f6726d6616418df3258278
tkyung05 <tkyung6761@gmail.com>
2022-07-07 22:30:21 +0900

    BOJ 11286 절대값 힙 : heapq 모듈 사용

--- code.py
+++ code.py
@@ -1,44 +1,20 @@
-from collections import deque
+import heapq
+import sys
 
-w, h = map(int, input().split())
+n = int(sys.stdin.readline())
 
-graph = []
-for _ in range(h):
-    graph.append(list(input()))
-
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-visited = [[False] * w for _ in range(h)]
-
-def bfs(y, x, team):
-    queue = deque([(y, x)])
-    visited[y][x] = True
-    count = 1
-
-    while queue:
-        y, x = queue.popleft()
-
-        for i in range(4):
-            ny, nx = y + dy[i], x + dx[i]
-            if ny < 0 or ny >= h or nx < 0 or nx >= w:
-                continue
-            if team == graph[ny][nx] and not visited[ny][nx]:
-                queue.append((ny, nx))
-                visited[ny][nx] = True
-                count += 1
-    return count ** 2
-
-W_result = 0
-B_result = 0
-
-for i in range(h):
-    for j in range(w):
-        if not visited[i][j]:
-            if graph[i][j] == 'W':
-                W_result += bfs(i, j, graph[i][j])
-            elif graph[i][j] == 'B':
-                B_result += bfs(i, j, graph[i][j])
-
-print(W_result, B_result)
-
+heap = []
+for _ in range(n):
+    num = int(sys.stdin.readline())
+    
+    if num == 0:
+        if not heap:
+            print(0)
+        else: 
+            print(heapq.heappop(heap)[1])
+    else:
+        heapq.heappush(heap, (abs(num), num))
+        
+        
+    
+    
