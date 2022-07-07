9e1cfd78c3353c92807a1713b32f2cd527107baf
tkyung05 <tkyung6761@gmail.com>
2022-07-07 15:11:26 +0900

    BOJ 1303 전쟁-전투 : well know 문제 bfs

--- code.py
+++ code.py
@@ -1,29 +1,44 @@
 from collections import deque
 
-n = int(input())
-cur_x, cur_y, dis_x, dis_y = map(int, input().split())
+w, h = map(int, input().split())
 
-graph = [[0] * n for _ in range(n)]
+graph = []
+for _ in range(h):
+    graph.append(list(input()))
 
-dx = [-2, -2, 0, 0, 2, 2]
-dy = [-1, 1, -2, 2, -1, 1]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-def bfs():
-    queue = deque([(cur_x, cur_y)])
-    graph[cur_x][cur_y] = 1
-    
+visited = [[False] * w for _ in range(h)]
+
+def bfs(y, x, team):
+    queue = deque([(y, x)])
+    visited[y][x] = True
+    count = 1
+
     while queue:
-        x, y = queue.popleft()
-        if x == dis_x and y == dis_y:
-            return graph[x][y]  
+        y, x = queue.popleft()
 
-        for i in range(6):
-            nx, ny = x + dx[i], y + dy[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= n:
+        for i in range(4):
+            ny, nx = y + dy[i], x + dx[i]
+            if ny < 0 or ny >= h or nx < 0 or nx >= w:
                 continue
-            if graph[nx][ny] == 0:
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx, ny))
-    return 0
-            
-print(bfs() - 1)
+            if team == graph[ny][nx] and not visited[ny][nx]:
+                queue.append((ny, nx))
+                visited[ny][nx] = True
+                count += 1
+    return count ** 2
+
+W_result = 0
+B_result = 0
+
+for i in range(h):
+    for j in range(w):
+        if not visited[i][j]:
+            if graph[i][j] == 'W':
+                W_result += bfs(i, j, graph[i][j])
+            elif graph[i][j] == 'B':
+                B_result += bfs(i, j, graph[i][j])
+
+print(W_result, B_result)
+
