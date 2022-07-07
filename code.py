11ac12b31dc9c88eeaf60ecfbce5681a1f69548d
tkyung05 <tkyung6761@gmail.com>
2022-07-07 10:15:59 +0900

    BOJ 16948 데스 나이트

--- code.py
+++ code.py
@@ -1,58 +1,29 @@
 from collections import deque
 
-h, w = map(int, input().split())
-graph = []
-for i in range(h):
-    graph.append(list(input()))
+n = int(input())
+cur_x, cur_y, dis_x, dis_y = map(int, input().split())
 
-dy = [-1, 1, 0, 0]
-dx = [0, 0, -1, 1]
-visited = [[False] * w for _ in range(h)]
+graph = [[0] * n for _ in range(n)]
 
-all_sheep = 0
-all_wolf = 0
+dx = [-2, -2, 0, 0, 2, 2]
+dy = [-1, 1, -2, 2, -1, 1]
 
-def bfs(y, x):
-    global all_sheep
-    global all_wolf
-
-    sheep = 0
-    wolf = 0
-    queue = deque([(y, x)])
-    visited[y][x] = True
-
-    while queue:
-        y, x = queue.popleft()
-        if graph[y][x] == 'o':
-            sheep += 1
-        elif graph[y][x] == 'v':
-            wolf += 1
-
-        for i in range(4):
-            ny, nx = y + dy[i], x + dx[i]
-            
-            if ny < 0 or ny >= h or nx < 0 or nx >= w:
-                continue
-            elif graph[ny][nx] == '.' and not visited[ny][nx]:
-                queue.append((ny, nx))
-                visited[ny][nx] = True
-
-            elif (graph[ny][nx] == 'o' or graph[ny][nx] == 'v') and not visited[ny][nx]:
-                queue.append((ny, nx))
-                visited[ny][nx] = True
+def bfs():
+    queue = deque([(cur_x, cur_y)])
+    graph[cur_x][cur_y] = 1
     
-    if sheep > wolf:
-        wolf = 0
-    elif sheep <= wolf:
-        sheep = 0
+    while queue:
+        x, y = queue.popleft()
+        if x == dis_x and y == dis_y:
+            return graph[x][y]  
 
-    all_sheep += sheep
-    all_wolf += wolf
-
-
-for i in range(h):
-    for j in range(w):
-        if (graph[i][j] == 'o' or graph[i][j] == 'v') and not visited[i][j]:
-            bfs(i, j)
-
-print(all_sheep, all_wolf)
+        for i in range(6):
+            nx, ny = x + dx[i], y + dy[i]
+            if nx < 0 or nx >= n or ny < 0 or ny >= n:
+                continue
+            if graph[nx][ny] == 0:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+    return 0
+            
+print(bfs() - 1)
