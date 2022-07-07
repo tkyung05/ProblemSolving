fa2eacb56ed7f59702ccd0d2a70bc75e60cfb1fc
tkyung05 <tkyung6761@gmail.com>
2022-07-08 08:21:54 +0900

    BOJ 13565 침투 : 무지성 bfs

--- code.py
+++ code.py
@@ -1,20 +1,44 @@
-import heapq
-import sys
+from collections import deque
 
-n = int(sys.stdin.readline())
+h, w = map(int, input().split())
 
-heap = []
-for _ in range(n):
-    num = int(sys.stdin.readline())
+graph = []
+for _ in range(h):
+    graph.append(list(map(int, input())))
+
+visited = [[False] * w for _ in range(h)]
+
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+def bfs(sy, sx):
+    queue = deque([(sy, sx)])
+    visited[sy][sx] = True
     
-    if num == 0:
-        if not heap:
-            print(0)
-        else: 
-            print(heapq.heappop(heap)[1])
-    else:
-        heapq.heappush(heap, (abs(num), num))
-        
-        
-    
-    
+    while queue:
+        y, x = queue.popleft()
+        for i in range(4):
+            ny, nx = y + dy[i], x + dx[i]
+            if ny < 0 or ny >= h or nx < 0 or nx >= w:
+                continue
+            if graph[ny][nx] == 0 and not visited[ny][nx]:
+                visited[ny][nx] = True
+                queue.append((ny, nx))
+
+for i in range(w):
+    if graph[0][i] == 0 and not visited[0][i]:
+        bfs(0, i)
+
+trig = False
+for i in range(w):
+    if graph[h-1][i] == 0 and visited[h-1][i]:
+        trig = True
+        break
+
+if trig:
+    print('YES')
+else:
+    print('NO')
+
+
+
