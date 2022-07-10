8d14aff5cc2d1721e5949a154591a24675db649f
tkyung05 <tkyung6761@gmail.com>
2022-07-10 10:43:24 +0900

    BOJ 1158 요세푸스 문제 : 큐 자료구조 사용

--- code.py
+++ code.py
@@ -1,70 +1,29 @@
-import sys
 from collections import deque
-input = sys.stdin.readline
 
-h, w = map(int, input().split())
+n, k = map(int, input().split())
 
-input_graph = []
-for _ in range(h):
-    input_graph.append(list(map(int, input().split())))
+result = [0] * n
+data = deque()
 
+for i in range(1, n + 1):
+    data.append(i)
 
-dy = [1, -1, 0, 0]
-dx = [0, 0, -1, 1]
-
-def ice_berg_search(graph):
-    temp_graph = [[0] * w for _ in range(h)]
-
-    for i in range(h):
-        for j in range(w):
-            if graph[i][j] != 0:
-                count = 0
-                for k in range(4):
-                    ny, nx = i + dy[k], j + dx[k]
-                    if graph[ny][nx] == 0:
-                        count += 1
-                cur_ice = graph[i][j] - count
-                if cur_ice < 0:
-                    cur_ice = 0
-                
-                temp_graph[i][j] = cur_ice
-
-    return temp_graph
-
-def bfs(y, x, graph, visited):
-    queue = deque([(y, x)])
-    visited[y][x] = True
-
-    while queue:
-        y, x = queue.popleft()
-        for i in range(4):
-            ny, nx = y + dy[i], x + dx[i]
-            if graph[ny][nx] != 0 and not visited[ny][nx]:
-                visited[ny][nx] = True
-                queue.append((ny, nx))
-    return visited
-
-
-check_graph = ice_berg_search(input_graph)
-result = 1
-
-while True:
-    visited = [[False] * w for _ in range(h)]
-    count = 0
-
-    for i in range(h):
-        for j in range(w):
-            if check_graph[i][j] != 0 and not visited[i][j]:
-                visited = bfs(i, j, check_graph, visited)
-                count += 1
-    
-    if count > 1:
-        print(result)
+for i in range(n):
+    if i == n - 1:
+        result[i] = data.popleft()
         break
-    elif count == 0:
-        print(0)
-        break
+         
+    for j in range(k):
+        pop_n = data.popleft()
+        if j == k - 1:
+            result[i] = pop_n
+            break
+        data.append(pop_n)
+
+print('<', end='')
+for i in range(len(result)):
+    if i == len(result) - 1:
+        print(f'{result[i]}', end='')
     else:
-        check_graph = ice_berg_search(check_graph)
-        result += 1 
-    
+        print(f'{result[i]},', end=' ')
+print('>', end='')
