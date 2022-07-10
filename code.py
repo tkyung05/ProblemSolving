21f5911c92d184f4e65ceb5ca754ce607cd1de2a
tkyung05 <tkyung6761@gmail.com>
2022-07-10 16:20:44 +0900

    BOJ 2206 벽 부수고 이동하기 : 3차원 리스트로 벽 부숨의 유무를 체크하여 bfs를 돌림

--- code.py
+++ code.py
@@ -1,29 +1,40 @@
 from collections import deque
 
-n, k = map(int, input().split())
+h, w = map(int, input().split())
 
-result = [0] * n
-data = deque()
+graph = []
+for _ in range(h):
+    graph.append(list(map(int, input())))
+        
+visited = [[[0] * 2 for _ in range(w)] for _ in range(h)]
 
-for i in range(1, n + 1):
-    data.append(i)
+dy = [1, -1, 0, 0]
+dx = [0, 0, -1, 1]
 
-for i in range(n):
-    if i == n - 1:
-        result[i] = data.popleft()
-        break
-         
-    for j in range(k):
-        pop_n = data.popleft()
-        if j == k - 1:
-            result[i] = pop_n
-            break
-        data.append(pop_n)
+def bfs():
+    queue = deque([(0, 0, 1)])
+    visited[0][0][1] = 1
 
-print('<', end='')
-for i in range(len(result)):
-    if i == len(result) - 1:
-        print(f'{result[i]}', end='')
-    else:
-        print(f'{result[i]},', end=' ')
-print('>', end='')
+    while queue:
+        y, x, key = queue.popleft()
+
+        if y == h - 1 and x == w - 1:
+            return visited[y][x][key]
+
+        for i in range(4):
+            ny, nx = y + dy[i], x + dx[i]
+            if ny < 0 or ny >= h or nx < 0 or nx >= w:
+                continue
+            
+            if graph[ny][nx] == 1 and key == 1:
+                visited[ny][nx][0] = visited[y][x][1] + 1
+                queue.append((ny, nx, 0))
+
+            elif graph[ny][nx] == 0 and visited[ny][nx][key] == 0:
+                visited[ny][nx][key] = visited[y][x][key] + 1
+                queue.append((ny, nx, key))
+
+    return -1
+    
+print(bfs())
+    
