4c1d08a59b295d0496cbe15b3253425fa2268fdd
tkyung05 <tkyung6761@gmail.com>
2022-07-12 08:51:50 +0900

    BOJ 11404 플로이드 : 플로이드 워셜 최단경로 알고리즘 멀티 엣지 풀이

--- code.py
+++ code.py
@@ -1,38 +1,31 @@
-import heapq
 import sys
 input = sys.stdin.readline
 INF = int(1e9)
 
-v, e = map(int, input().split())
-start = int(input())
+v = int(input())
+e = int(input())
 
-graph = [[] for _ in range(v + 1)]
-for _ in range(e):
-    n1, n2, dis = map(int, input().split())
-    graph[n1].append((n2, dis))
-
-distance = [INF] * (v + 1)
-
-def dijkstra(start):
-    q = []
-    distance[start] = 0
-    heapq.heappush(q, (0, start))
-
-    while q:
-        dis, node = heapq.heappop(q)
-
-        if distance[node] < dis:
-            continue
-        for i in graph[node]:
-            cost = dis + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
-
-dijkstra(start)
+distance_graph = [[INF] * (v + 1) for _ in range(v + 1)]
 
 for i in range(1, v + 1):
-    if distance[i] != INF:
-        print(distance[i])
-    else:
-        print('INF')
+    for j in range(1, v + 1):
+        if i == j:
+            distance_graph[i][j] = 0
+            break
+
+for _ in range(e):
+    n1, n2, dis = map(int, input().split())
+    distance_graph[n1][n2] = min(distance_graph[n1][n2], dis)
+
+for k in range(1, v + 1):
+    for i in range(1, v + 1):
+        for j in range(1, v + 1):
+            distance_graph[i][j] = min(distance_graph[i][j], distance_graph[i][k] + distance_graph[k][j])
+
+for i in range(1, v + 1):
+    for j in range(1, v + 1):
+        if distance_graph[i][j] >= INF:
+            print(0, end=' ')
+        else:
+            print(distance_graph[i][j], end=' ')
+    print()
