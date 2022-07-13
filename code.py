72e05dc336cc03f1768bc024bc5b9f35d388e642
tkyung05 <tkyung6761@gmail.com>
2022-07-13 22:24:33 +0900

    BOJ 15903 카드 합체 놀이 : 날먹

--- code.py
+++ code.py
@@ -1,34 +1,21 @@
-import sys
 import heapq
+import sys
 input = sys.stdin.readline
-INF = int(1e9)
 
-v = int(input())
-e = int(input())
+n, m = map(int, input().split())
 
-graph = [[] for _ in range(v + 1)]
-distance = [INF] * (v + 1) 
+temp = list(map(int, input().split()))
 
-for _ in range(e):
-    n1, n2, dis = map(int, input().split())
-    graph[n1].append((n2, dis))
+card = []
+for i in temp:
+    heapq.heappush(card, i)
 
-start_v, target_v = map(int, input().split())
+for i in range(m):
+    a = heapq.heappop(card)
+    b = heapq.heappop(card)
 
-def dijkstra(start):
-    q = []
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
-    
-    while q:
-        dis, node = heapq.heappop(q)
-        if distance[node] < dis:
-            continue
-        for i in graph[node]:
-            cost = dis + i[1]
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+    sum_num = a + b
+    heapq.heappush(card, sum_num)
+    heapq.heappush(card, sum_num)
 
-dijkstra(start_v)
-print(distance[target_v])
+print(sum(card))
