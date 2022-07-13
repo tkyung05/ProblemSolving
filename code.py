98712cd3d296cb3f7c445b3388f62274bbd02a47
tkyung05 <tkyung6761@gmail.com>
2022-07-13 11:48:57 +0900

    BOJ 18405 경쟁적 전염 : bfs 복귀

--- code.py
+++ code.py
@@ -1,40 +1,41 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
-INF = int(1e9)
 
-v, e = map(int, input().split())
+n, k = map(int, input().split())
 
-graph = [[INF] * (v + 1) for _ in range(v + 1)]
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
 
-for i in range(1, v + 1):
-    for j in range(1, v + 1):
-        if i == j:
-            graph[i][j] = 0
+temp = []
+for i in range(n):
+    for j in range(n):
+        if graph[i][j] != 0:
+            temp.append((graph[i][j], 0, i, j))
+
+temp.sort()
+queue = deque(temp)
+
+time, dist_y, dist_x = map(int, input().split())
+
+dy = [-1, 1, 0, 0]
+dx = [0, 0, -1, 1]
+
+def bfs():
+    while queue:
+        virus_num, sec, y, x = queue.popleft()
+
+        if sec == time:
             break
 
-for _ in range(e):
-    n1, n2 = map(int, input().split())
-    graph[n1][n2] = 1
-    graph[n2][n1] = 1 
+        for i in range(4):
+            ny, nx = y + dy[i], x + dx[i]
+            if ny < 0 or ny >= n or nx < 0 or nx >= n:
+                continue
+            if graph[ny][nx] == 0:
+                graph[ny][nx] = virus_num
+                queue.append((virus_num, sec + 1, ny, nx)) 
+bfs()
+print(graph[dist_y - 1][dist_x - 1])
 
-for k in range(1, v + 1):
-    for i in range(1, v + 1):
-        for j in range(1, v + 1):
-            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
-
-result = []
-
-for i in range(1, v + 1):
-    count = 0
-    for j in range(1, v + 1):
-        count += graph[i][j]
-    result.append((count, i))
-
-result.sort()
-
-print(result[0][1])
-
-
-
-
-    
