53e6d93e913fb40701863984fa119053c62bb0ff
tkyung05 <tkyung6761@gmail.com>
2022-07-15 22:18:42 +0900

    BOJ 1202 보석 도둑 : 내 코드는 TLE 나서 max heap 방식으로 풀음.

--- code.py
+++ code.py
@@ -1,34 +1,28 @@
 import sys
+import heapq
 input = sys.stdin.readline
 
-limit = 1000000
-eratos_data = [True] * (limit + 1)
-eratos_data[0], eratos_data[1] = False, False
+n, k = map(int, input().split())
 
-for i in range(2, int(len(eratos_data) ** 0.5)):
-    if eratos_data[i]:
-        for j in range(i + i, len(eratos_data), i):
-            eratos_data[j] = False
+jewels = [[] for _ in range(n)]
+for i in range(n):
+    jewels[i] = list(map(int, input().split()))
 
-prime_data = []
-for i in range(2, len(eratos_data)):
-    if eratos_data[i]:
-        prime_data.append(i)
+bags = [0] * k
+for i in range(k):
+    bags[i] = int(input())
 
-while True:
-    n = int(input())
+jewels.sort()
+bags.sort()
 
-    if n == 0:
-        break
-    
-    bug = True
-    for i in prime_data:
-        back = n - i
-        if eratos_data[back] and back % 2 != 0:
-            front = i
-            bug = False
-            break
-    if bug:
-        prime_data('Goldbach\'s conjecture is wrong.')
-    else:
-        print(f'{n} = {front} + {back}')
+result = 0
+choice_jews = []
+
+for bag in bags:
+    while jewels and bag >= jewels[0][0]:
+        heapq.heappush(choice_jews, heapq.heappop(jewels)[1] * -1)
+        
+    if choice_jews:
+        result += heapq.heappop(choice_jews)
+
+print(result * -1)
