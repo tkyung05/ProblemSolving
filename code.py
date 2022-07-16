1e15a131e19a35254245064e72204ccef7e53ffa
tkyung05 <tkyung6761@gmail.com>
2022-07-16 15:07:06 +0900

    BOJ 1092 배 : 아이디어 떠올리는건 쉬웠는데 코드를 너무 어렵게 생각했음.

--- code.py
+++ code.py
@@ -1,28 +1,25 @@
 import sys
-import heapq
 input = sys.stdin.readline
 
-n, k = map(int, input().split())
+n = int(input())
+crains = list(map(int, input().split()))
+k = int(input())
+boxs = list(map(int, input().split()))
 
-jewels = [[] for _ in range(n)]
-for i in range(n):
-    jewels[i] = list(map(int, input().split()))
-
-bags = [0] * k
-for i in range(k):
-    bags[i] = int(input())
-
-jewels.sort()
-bags.sort()
+crains.sort(reverse=True)
+boxs.sort(reverse=True)
 
 result = 0
-choice_jews = []
+if crains[0] < boxs[0]:
+    result = -1
+else:
+    while boxs:
+        result += 1
 
-for bag in bags:
-    while jewels and bag >= jewels[0][0]:
-        heapq.heappush(choice_jews, heapq.heappop(jewels)[1] * -1)
-        
-    if choice_jews:
-        result += heapq.heappop(choice_jews)
-
-print(result * -1)
+        for c in crains:
+            for i in range(len(boxs)):
+                if c >= boxs[i]:
+                    boxs.pop(i)
+                    break
+            
+print(result)
