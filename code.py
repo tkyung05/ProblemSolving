1f7c59697074d20c02873d2f5683a00c50627712
tkyung05 <tkyung6761@gmail.com>
2022-07-17 11:15:16 +0900

    BOJ 1707 이분 그래프 : 그래프에서 인접한 모든 노드들 끼리 서로 다른 두가지 색으로만 나타낼 수 있을때 그 그래프를 이분 그래프라 한다.

--- code.py
+++ code.py
@@ -1,20 +1,48 @@
-import heapq
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-n = int(input())
+T = int(input())
 
-test_score = []
-for _ in range(n):
-    test_score.append(tuple(map(int, input().split())))
+def bfs(n):
+    q = deque([n])
+    visited[n] = 1
 
-test_score.sort()
+    while q:
+        n = q.popleft()
 
-result = []
-for day, score in test_score:
-    heapq.heappush(result, score)
+        for i in graph[n]:
+            if visited[i] == 0:
+                visited[i] = visited[n] * -1
+                q.append(i)
+            else:
+                if visited[n] == visited[i]:
+                    return False
+    return True
 
-    if len(result) > day:
-        heapq.heappop(result)
 
-print(sum(result))
+for _ in range(T):
+    v, e = map(int, input().split())
+
+    visited = [0] * (v + 1)
+    graph = [[] for _ in range(v + 1)]
+
+    for _ in range(e):
+        n1, n2 = map(int, input().split())
+        graph[n1].append(n2)
+        graph[n2].append(n1)
+
+    bug = False
+
+    for i in range(1, v + 1):
+        if visited[i] == 0:
+            if not bfs(i):
+                bug = True
+                break
+    
+    if bug:
+        print('NO')
+    else:
+        print('YES')
+
+    
