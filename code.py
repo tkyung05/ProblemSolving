c522695c82c2f901eabae6908677f5a41f9cc5f6
tkyung05 <tkyung6761@gmail.com>
2022-07-20 00:36:56 +0900

    BOJ 4179 불! : bfs 탈출 문제 G4 웰 노운인데 급하게 하다가 실수했음.

--- code.py
+++ code.py
@@ -1,39 +1,40 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
 dy = [-1, 1, 0, 0]
 dx = [0, 0, -1, 1]
 
 def bfs(y, x):
     q.appendleft((y, x))
-    visited[y][x][0] = 1 # 방문 체크 카운트
-    visited[y][x][1] = 0 # 상태 값 : 다람쥐 = 0
+    visited[y][x][0] = 1
 
     while q:
         y, x = q.popleft()
+
+        if (y == (h - 1) or x == (w - 1) or y == 0 or x == 0) and graph[y][x] == '.' and visited[y][x][1] == 0:
+            return visited[y][x][0]
 
         for i in range(4):
             ny, nx = y + dy[i], x + dx[i]
 
             if ny < 0 or ny >= h or nx < 0 or nx >= w:
                 continue
-
-            if graph[ny][nx] == 'D' and visited[y][x][1] == 0:
-                return visited[y][x][0]
             
-            # 자신이 물인 경우
-            if graph[ny][nx] == '.' and visited[y][x][1] == 1 and visited[ny][nx][0] >= 0:
-                visited[ny][nx][0] = -1
-                visited[ny][nx][1] = 1
-                q.append((ny, nx))
-            
-            # 자신이 다람쥐인 경우
-            if graph[ny][nx] == '.' and visited[y][x][1] == 0 and visited[y][x][0] > 0 and visited[ny][nx][0] == 0:
+            # 지훈이인 경우
+            if visited[y][x][1] == 0 and graph[ny][nx] == '.' and visited[ny][nx][0] == 0:
                 visited[ny][nx][0] = visited[y][x][0] + 1
-                visited[ny][nx][1] = 0
                 q.append((ny, nx))
-     
-    return 'KAKTUS'
+
+            # 불인 경우
+            elif visited[y][x][1] == 1 and graph[ny][nx] == '.' and visited[ny][nx][0] >= 0:
+                visited[ny][nx][1] = 1
+                visited[ny][nx][0] = -1
+                q.append((ny, nx))
+    
+    return 'IMPOSSIBLE'
                 
+
 
 h, w = map(int, input().split())
 
@@ -43,17 +44,17 @@ for _ in range(h):
 
 visited = [[[0] * 2 for _ in range(w)] for _ in range(h)]
 
-s_y, s_x = 0, 0
 q = deque()
+j_y, j_x = 0, 0
 
 for i in range(h):
     for j in range(w):
-        if graph[i][j] == '*':
+        if graph[i][j] == 'F':
             q.append((i, j))
-            visited[i][j][0] = -1 # 방문 체크
-            visited[i][j][1] = 1 # 상태 값 : 물 = 1, 다람쥐 = 0 
-        elif graph[i][j] == 'S':
+            visited[i][j][0] = -1 # 방문 처리
+            visited[i][j][1] = 1 # 1 = 불, 0 = 지훈
+        elif graph[i][j] == 'J':
+            j_y, j_x = i, j
             graph[i][j] = '.'
-            s_y, s_x = i, j
 
-print(bfs(s_y, s_x))
+print(bfs(j_y, j_x))
