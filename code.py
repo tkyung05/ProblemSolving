694d21c95a933c0a3c41b7153bb785a705dfeddc
tkyung05 <tkyung6761@gmail.com>
2022-07-20 23:38:56 +0900

    BOJ 3088 화분 부수기 : 문제를 잘 못 읽고 무지성 이중 포문 갈겼다가 틀림 맞고, 알고보니 시간복잡도 + 문제 알고리즘상 dp 테이블 사용하여 그리디하게 푸는 문제였음.

--- code.py
+++ code.py
@@ -1,60 +1,18 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
-dy = [-1, 1, 0, 0]
-dx = [0, 0, -1, 1]
+n = int(input())
+data = []
+for _ in range(n):
+    data.append(list(map(int, input().split())))
 
-def bfs(y, x):
-    q.appendleft((y, x))
-    visited[y][x][0] = 1
+visited = [False] * 1000001
+count = 0
+for a, b, c in data:
+    if not visited[a] and not visited[b] and not visited[c]:
+        count += 1
+    visited[a] = True
+    visited[b] = True
+    visited[c] = True
 
-    while q:
-        y, x = q.popleft()
-
-        if (y == (h - 1) or x == (w - 1) or y == 0 or x == 0) and graph[y][x] == '.' and visited[y][x][1] == 0:
-            return visited[y][x][0]
-
-        for i in range(4):
-            ny, nx = y + dy[i], x + dx[i]
-
-            if ny < 0 or ny >= h or nx < 0 or nx >= w:
-                continue
-            
-            # 지훈이인 경우
-            if visited[y][x][1] == 0 and graph[ny][nx] == '.' and visited[ny][nx][0] == 0:
-                visited[ny][nx][0] = visited[y][x][0] + 1
-                q.append((ny, nx))
-
-            # 불인 경우
-            elif visited[y][x][1] == 1 and graph[ny][nx] == '.' and visited[ny][nx][0] >= 0:
-                visited[ny][nx][1] = 1
-                visited[ny][nx][0] = -1
-                q.append((ny, nx))
-    
-    return 'IMPOSSIBLE'
-                
-
-
-h, w = map(int, input().split())
-
-graph = []
-for _ in range(h):
-    graph.append(list(map(str, input().strip('\n'))))
-
-visited = [[[0] * 2 for _ in range(w)] for _ in range(h)]
-
-q = deque()
-j_y, j_x = 0, 0
-
-for i in range(h):
-    for j in range(w):
-        if graph[i][j] == 'F':
-            q.append((i, j))
-            visited[i][j][0] = -1 # 방문 처리
-            visited[i][j][1] = 1 # 1 = 불, 0 = 지훈
-        elif graph[i][j] == 'J':
-            j_y, j_x = i, j
-            graph[i][j] = '.'
-
-print(bfs(j_y, j_x))
+print(count)
