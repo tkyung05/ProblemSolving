e8128ed2303b904345f05b47c11cdb53a4f7d9cd
tkyung05 <tkyung6761@gmail.com>
2022-07-21 15:33:09 +0900

    BOJ 13913 숨바꼭질 4 : 숨바꼭질 문제 All solved. 부모 노드를 메모 하는 방식으로 경로를 출력할 수 있다는 것을 배움.

--- code.py
+++ code.py
@@ -2,32 +2,40 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-result = 0
-
 def bfs(n):
-    global result
     q = deque([(n)])
     graph[n] = 1
+    move[n] = -1
 
     while q:
         x = q.popleft()
 
         if x == k:
-            result += 1
+            return graph[x] - 1
 
         for nx in (x*2, x+1, x-1):
             if nx < 0 or nx > 100000:
                 continue
     
-            if graph[nx] == 0 or graph[nx] == graph[x] + 1:
+            if graph[nx] == 0:
                 graph[nx] = graph[x] + 1
+                move[nx] = x
                 q.append(nx)
 
 
 n, k = map(int, input().split())
 graph = [0] * 100001
+move = [0] * 100001
 
-bfs(n)
+print(bfs(n))
 
-print(graph[k] - 1)
-print(result)
+move_data = []
+while True:
+    if k == -1:
+        break
+    move_data.append(k)
+    k = move[k]
+
+for i in reversed(move_data):
+    print(i, end=' ')
+
