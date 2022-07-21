fa00ad4cd3ec1f98f74187b85b245c4337283bf0
tkyung05 <tkyung6761@gmail.com>
2022-07-21 14:05:12 +0900

    BOJ 13549 숨바꼭질 3 : 골드5 bfs 웰 노운 문제 원 큐만에 성공 ㅋ.

--- code.py
+++ code.py
@@ -1,26 +1,33 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-while True:
-    try:
-        n = int(input())
-    except:
-        break
+def bfs(n):
+    q = deque([(n)])
+    graph[n] = 1
 
-    if n == 1:
-        print(1)
-        continue
+    while q:
+        x = q.popleft()
 
-    one_num = 1
-    while True:
-        one_num = one_num * 10 + 1
-        
-        if one_num % n == 0:
-            break 
-    
-    print(len(str(one_num)))
-    
+        if x == k:
+            return graph[x] - 1
 
+        tel_x = x * 2
+        if tel_x <= 100000:
+            graph[tel_x] = graph[x]
+            q.append(tel_x)
+
+        for nx in (x-1, x+1):
+            if nx < 0 or nx > 100000:
+                continue
+            if graph[nx] == 0:
+                graph[nx] = graph[x] + 1
+                q.append(nx)
         
-        
+
+n, k = map(int, input().split())
+graph = [0] * 100001
+
+print(bfs(n))
+
 
