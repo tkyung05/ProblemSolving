34acb6b38d30a0d705869950aed8b4b0d5ce6ed5
tkyung05 <tkyung6761@gmail.com>
2022-07-21 18:18:08 +0900

    BOJ 2847 게임을 만든 동준이 : 오랜만에 실버4 문제 골드는 어려워서 ez한 실버 그리디 풀기..!..

--- code.py
+++ code.py
@@ -1,48 +1,21 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
-limit = 100000
 
-dx = [1, 2]
+n = int(input())
 
-def bfs(n):
-    q = deque([(n)])
-    graph[n] = 1
-
-    while q:
-        x = q.popleft()
-
-        if graph[x] - 1 > t:
-            return 'ANG'
-        if x == g:
-            return graph[x] - 1 
-
-        for i in range(2):
-            if i == 0:
-                btn = x + dx[i]
-                if btn >= limit:
-                    continue
-                
-            elif i == 1:
-                btn = x * dx[i]
-                if btn >= limit:
-                    continue
-                if btn > 0 and btn < 10:
-                    btn -= 1
-                elif btn > 0:
-                    btn = btn - (10 ** (len(str(btn))-1))
-
-            if graph[btn] == 0:
-                graph[btn] = graph[x] + 1
-                q.append(btn)
-
-    return 'ANG'
-                
-
-    
+data = [0] * n
+for i in range(n):
+    data[i] = int(input())
+data.reverse()
 
 
-graph = [0] * limit
-n, t, g = map(int, input().split())
+result = 0
+for i in range(n - 1):
+    cur, pront = data[i], data[i + 1]
 
-print(bfs(n))
+    if cur <= pront:
+        min_gap = pront - cur + 1
+        result += min_gap
+        data[i + 1] = pront - min_gap
+
+print(result)
