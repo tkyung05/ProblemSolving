fe249455714f8135528e89b47c142243568c1e87
tkyung05 <tkyung6761@gmail.com>
2022-07-22 14:39:43 +0900

    BOJ 1018 체스판 다시 칠하기 : 아이디어는 간단한 브루트포스 문제인데 구현하는데 꽤 힘들었음. 근데 다른 사람 풀이 보다 더 효율적으로 푼 것 같아서 기분 좋음.

--- code.py
+++ code.py
@@ -1,21 +1,56 @@
 import sys
 input = sys.stdin.readline
 
-e, s, m = map(int, input().split())
+h, w = map(int, input().split())
+graph = []
+for _ in range(h):
+    graph.append(list(map(str, input().strip('\n'))))
 
-result = 0
-limit = [15, 28, 19]
-data = [0, 0, 0]
+
+
+b_and_w = [['B', 'W'], ['W', 'B']]
+result = 64
+
+def search_chess():
+    global result
+
+    b_count, w_count = 0, 0
+    b_f, s = 1, 0
+
+    for i in range(8):
+        if b_f == 1: b_f, w_f = 0, 1
+        else: b_f, w_f = 1, 0
+
+        for j in range(8):
+            if board[i][j] != b_and_w[b_f][s]:
+                b_count += 1
+            if board[i][j] != b_and_w[w_f][s]:
+                w_count += 1
+
+            if s == 1: s = 0
+            else: s = 1
+            
+    result = min(result, b_count, w_count)
+
+
+limit_h, limit_w = h - 8, w - 8
+add_h, add_w = 0, 0
+
+board = [[' '] * 8 for _ in range(8)]
 
 while True:
-    if data[0] == e and data[1] == s and data[2] == m:
-        break
-    
-    for i in range(3):
-        data[i] = data[i] + 1
-        if data[i] > limit[i]:
-            data[i] = 1
 
-    result += 1
+    for i in range(8):
+        for j in range(8):
+            board[i][j] = graph[i + add_h][j + add_w]     
+    search_chess()
+
+    if add_w < limit_w:
+        add_w += 1
+    elif add_h < limit_h:
+        add_h += 1
+        add_w = 0
+    else:
+        break
 
 print(result)
