21b4fc5be1facf2b0db0a3db4e962ef33fe1e28d
tkyung05 <tkyung6761@gmail.com>
2022-07-22 16:39:40 +0900

    BOJ 123023 ABCDE : 오랜만에 DFS

--- code.py
+++ code.py
@@ -1,56 +1,40 @@
 import sys
+sys.setrecursionlimit(100000)
 input = sys.stdin.readline
 
-h, w = map(int, input().split())
-graph = []
-for _ in range(h):
-    graph.append(list(map(str, input().strip('\n'))))
+def dfs(node, count):
+    global check
+
+    if count == 5:
+        check = True
+        return
+    
+    visited[node] = True
+    
+    for i in graph[node]:
+        if not visited[i]:
+            dfs(i, count + 1)
+            visited[i] = False
 
 
+n, e = map(int, input().split())
 
-b_and_w = [['B', 'W'], ['W', 'B']]
-result = 64
+graph = [[] for _ in range(n)]
+for _ in range(e):
+    n1, n2 = map(int, input().split())
+    graph[n1].append(n2)
+    graph[n2].append(n1)
 
-def search_chess():
-    global result
+check = False
+visited = [False] * n
 
-    b_count, w_count = 0, 0
-    b_f, s = 1, 0
-
-    for i in range(8):
-        if b_f == 1: b_f, w_f = 0, 1
-        else: b_f, w_f = 1, 0
-
-        for j in range(8):
-            if board[i][j] != b_and_w[b_f][s]:
-                b_count += 1
-            if board[i][j] != b_and_w[w_f][s]:
-                w_count += 1
-
-            if s == 1: s = 0
-            else: s = 1
-            
-    result = min(result, b_count, w_count)
-
-
-limit_h, limit_w = h - 8, w - 8
-add_h, add_w = 0, 0
-
-board = [[' '] * 8 for _ in range(8)]
-
-while True:
-
-    for i in range(8):
-        for j in range(8):
-            board[i][j] = graph[i + add_h][j + add_w]     
-    search_chess()
-
-    if add_w < limit_w:
-        add_w += 1
-    elif add_h < limit_h:
-        add_h += 1
-        add_w = 0
-    else:
+for i in range(n):
+    if check:
         break
+    dfs(i, 1)
+    visited[i] = False
 
-print(result)
+if check:
+    print(1)
+else:
+    print(0)
