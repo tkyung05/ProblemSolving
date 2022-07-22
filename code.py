d7e433b4ea8227a26ffbab8df5335f79ccccc26c
tkyung05 <tkyung6761@gmail.com>
2022-07-22 12:37:49 +0900

    BOJ 1476 날짜 계산 : 브루트포스는 무식해.... 내 뇌가 굳는 느낌이야..

--- code.py
+++ code.py
@@ -1,44 +1,21 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
+e, s, m = map(int, input().split())
 
-graph = []
-for _ in range(n):
-    graph.append(list(map(str, input().strip('\n'))))
+result = 0
+limit = [15, 28, 19]
+data = [0, 0, 0]
 
-max_result = 1
+while True:
+    if data[0] == e and data[1] == s and data[2] == m:
+        break
+    
+    for i in range(3):
+        data[i] = data[i] + 1
+        if data[i] > limit[i]:
+            data[i] = 1
 
-def search():
-    global max_result
+    result += 1
 
-    for i in range(n):
-        w_count = 1
-        h_count = 1
-        for j in range(n - 1):
-            if graph[i][j] == graph[i][j + 1]:
-                w_count += 1
-                max_result = max(max_result, w_count)
-            if graph[i][j] != graph[i][j + 1]:
-                w_count = 1
-
-            if graph[j][i] == graph[j + 1][i]:
-                h_count += 1
-                max_result = max(max_result, h_count)
-            if graph[j][i] != graph[j + 1][i]:
-                h_count = 1
-
-
-for i in range(n):
-    for j in range(n - 1):
-        if graph[i][j] != graph[i][j + 1]:
-            graph[i][j], graph[i][j + 1] = graph[i][j + 1], graph[i][j]
-            search()
-            graph[i][j], graph[i][j + 1] = graph[i][j + 1], graph[i][j]
-
-        if graph[j][i] != graph[j + 1][i]:
-            graph[j][i], graph[j + 1][i] = graph[j + 1][i], graph[j][i]
-            search()
-            graph[j][i], graph[j + 1][i] = graph[j + 1][i], graph[j][i]
-
-print(max_result)
+print(result)
