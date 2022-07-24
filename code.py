e9c6090cb2548f6485a9d19d8af9114ec26e218f
tkyung05 <tkyung6761@gmail.com>
2022-07-24 23:45:31 +0900

    BOJ 1748  수 이어 쓰기 1 : 내가 최소한의 수학적 사고는 있구나. 라고 생각했다.

--- code.py
+++ code.py
@@ -2,28 +2,18 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-alpa_data = [list(map(str, input().strip('\n'))) for _ in range(n)]
 
-dict_alpa = {}
+leng = len(str(n))
+nine_str = '9'
+zero_str = '0'
 
-# 각 알파벳의 현재 위치에 따라 10의 제곱으로 크기를 정하고, 알파벳의 종류에 따라 dict에 저장 또는 더함
-for word in alpa_data:
-    leng = len(word) - 1
+if leng == 1:
+    print(n)
+    exit(0)
+ 
+result = (n - int((leng - 1) * nine_str)) * leng + 9
 
-    for k in word:
-        if k in dict_alpa:
-            dict_alpa[k] += 10 ** leng
-        else:
-            dict_alpa[k] = 10 ** leng
-        leng -= 1
-
-# 위에서 정한 값이 클 수록 우선 순위가 높아지기 때문에 내림차순 정렬
-data = sorted(dict_alpa.values(), reverse=True)
-
-# 우선 순위에 따라 9, 8, 7... 순으로 곱해줌
-result, num = 0, 9
-for i in data:
-    result += i * num
-    num -= 1
+for i in range(1, leng - 1):
+    result += int(nine_str + (zero_str * i)) * (i + 1)
 
 print(result)
