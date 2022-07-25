72d8d30b52980ff73e95990f16170e8fe72865d5
tkyung05 <tkyung6761@gmail.com>
2022-07-25 14:28:42 +0900

    BOJ 10816 숫자 카드 2 : 이진 탐색 실버 문제.

--- code.py
+++ code.py
@@ -1,19 +1,41 @@
 import sys
 input = sys.stdin.readline
 
+def bin_search(target): 
+    left, right = 0, m - 1
+    mid = (left + right) // 2
+
+    while left <= right:
+        if search_list[mid] == target:
+            return True
+
+        if search_list[mid] < target:
+            left = mid + 1
+        elif search_list[mid] > target:
+            right = mid - 1
+            
+        mid = (right + left) // 2
+
+    return False
+
+        
 n = int(input())
+card_list = list(map(int, input().split()))
+m = int(input())
+search = list(map(int, input().split()))
 
-leng = len(str(n))
-nine_str = '9'
-zero_str = '0'
+search_list = sorted(search)
 
-if leng == 1:
-    print(n)
-    exit(0)
- 
-result = (n - int((leng - 1) * nine_str)) * leng + 9
+result = {}
+for i in range(n):
+    if bin_search(card_list[i]):
+        if card_list[i] in result:
+            result[card_list[i]] += 1
+        else:
+            result[card_list[i]] = 1
 
-for i in range(1, leng - 1):
-    result += int(nine_str + (zero_str * i)) * (i + 1)
-
-print(result)
+for c in search:
+    if c in result:
+        print(result[c], end=' ')
+    else:
+        print(0, end=' ')
