1b08912783135c60c566d212d2865ad7e07a8f94
tkyung05 <tkyung6761@gmail.com>
2022-07-25 22:11:02 +0900

    BOJ 15649 N과M(1) : 백트랙킹 어렵구만 그래도 백트랙킹 중요하다고 하니까 해보자!

--- code.py
+++ code.py
@@ -1,20 +1,23 @@
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit = 10 ** 5
 
-n = int(input())
-dp = [0] * (n + 1) # (1 ~ n)각 수의 최소 연산 횟수를 저장할 메모장
+n, m = map(int, input().split())
 
-# 상향식 이유 => (1부터 n까지, n부터 1까지) 연산 횟수는 동일하기 때문. 
-for i in range(2, n + 1):
-    # i에 도달 하기 위한 최소 연산 횟수를 구할 수 있음.
-    # 점화식 : min(dp[i - 1] + 1, dp[i // 2] + 1, dp[i // 3] + 1)
+result = []
+visited = [False] * (n + 1)
+
+def dfs(count):
+    if count == m:
+        print(' '.join(list(map(str, result))))
+        return 
     
-    dp[i] = dp[i - 1] + 1
-    
-    if i % 3 == 0:
-        dp[i] = min(dp[i], dp[i // 3] + 1)
-        
-    if i % 2 == 0:
-        dp[i] = min(dp[i], dp[i // 2] + 1)
+    for i in range(1, n + 1):
+        if not visited[i]:
+            visited[i] = True
+            result.append(i)
+            dfs(count + 1)
+            result.pop()
+            visited[i] = False
 
-print(dp[n])
+dfs(0)
