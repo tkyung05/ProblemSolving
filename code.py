11de9734148dfc5eecfaf54e720875b09ddca54b
tkyung05 <tkyung6761@gmail.com>
2022-07-25 15:53:16 +0900

    BOJ 1463 1로 만들기 : 이해 갈랑말랑.. DP 어려움.

--- code.py
+++ code.py
@@ -1,41 +1,20 @@
 import sys
 input = sys.stdin.readline
 
-def bin_search(target): 
-    left, right = 0, m - 1
-    mid = (left + right) // 2
-
-    while left <= right:
-        if search_list[mid] == target:
-            return True
-
-        if search_list[mid] < target:
-            left = mid + 1
-        elif search_list[mid] > target:
-            right = mid - 1
-            
-        mid = (right + left) // 2
-
-    return False
-
-        
 n = int(input())
-card_list = list(map(int, input().split()))
-m = int(input())
-search = list(map(int, input().split()))
+dp = [0] * (n + 1) # (1 ~ n)각 수의 최소 연산 횟수를 저장할 메모장
 
-search_list = sorted(search)
+# 상향식 이유 => (1부터 n까지, n부터 1까지) 연산 횟수는 동일하기 때문. 
+for i in range(2, n + 1):
+    # i에 도달 하기 위한 최소 연산 횟수를 구할 수 있음.
+    # 점화식 : min(dp[i - 1] + 1, dp[i // 2] + 1, dp[i // 3] + 1)
+    
+    dp[i] = dp[i - 1] + 1
+    
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i // 3] + 1)
+        
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i // 2] + 1)
 
-result = {}
-for i in range(n):
-    if bin_search(card_list[i]):
-        if card_list[i] in result:
-            result[card_list[i]] += 1
-        else:
-            result[card_list[i]] = 1
-
-for c in search:
-    if c in result:
-        print(result[c], end=' ')
-    else:
-        print(0, end=' ')
+print(dp[n])
