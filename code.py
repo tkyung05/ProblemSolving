283871dc5fd528502270b6377f9216dfda3165b5
tkyung05 <tkyung6761@gmail.com>
2022-07-25 22:52:54 +0900

    BOJ 15651 N과M(3) : 좀 날먹인 거 같은데 그래도 백트랙킹 뉴비한테 꽤 괜찮은 문제인듯

--- code.py
+++ code.py
@@ -5,19 +5,15 @@ sys.setrecursionlimit = 10 ** 5
 n, m = map(int, input().split())
 
 result = []
-visited = [False] * (n + 1)
 
 def dfs(count):
     if count == m:
         print(' '.join(list(map(str, result))))
         return 
     
-    for i in range(1, n + 1):
-        if not visited[i] and len(result) == 0 or result[-1] < i:
-            visited[i] = True
-            result.append(i)
-            dfs(count + 1)
-            result.pop()
-            visited[i] = False
+    for i in range(1, n + 1):            
+        result.append(i)
+        dfs(count + 1)
+        result.pop()
 
 dfs(0)
