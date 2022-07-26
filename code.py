e75065397be6c158c6d8ba37a88bed576625b99e
tkyung05 <tkyung6761@gmail.com>
2022-07-26 13:00:11 +0900

    BOJ 15654 N과M(5) : 오늘도 N과m

--- code.py
+++ code.py
@@ -3,7 +3,10 @@ input = sys.stdin.readline
 sys.setrecursionlimit = 10 ** 5
 
 n, m = map(int, input().split())
+data = list(map(int, input().split()))
+data.sort()
 
+visited = [False] * 10001
 result = []
 
 def dfs(count):
@@ -11,9 +14,14 @@ def dfs(count):
         print(' '.join(list(map(str, result))))
         return 
     
-    for i in range(1, n + 1):            
-        result.append(i)
-        dfs(count + 1)
-        result.pop()
+    for i in data:
+        if not visited[i]:
+            visited[i] = True
+            result.append(i)
+            dfs(count + 1)
+            
+            visited[i] = False
+            result.pop()
+
 
 dfs(0)
