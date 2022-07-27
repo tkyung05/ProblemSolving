4c9824d1fe7fe510da64924ae511fd5ff634add5
tkyung05 <tkyung6761@gmail.com>
2022-07-27 14:14:21 +0900

    BOJ 14888 연산자 끼워넣기

--- code.py
+++ code.py
@@ -1,26 +1,33 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit = 10 ** 5
 
-n, m = map(int, input().split())
-data = list(map(int, input().split()))
-data.sort()
+n = int(input())
+num_data = list(map(int, input().split()))
+op_data = list(map(int, input().split()))
 
-visited = [False] * 10001
-result = []
+min_result = 1e9
+max_result = -1e9
 
-def dfs(count):
-    if count == m:
-        print(' '.join(list(map(str, result))))
-        return 
+def dfs(count, total, plus, minus, mul, div):
+    global min_result
+    global max_result
+
+    if count == n:
+        min_result = min(min_result, total)
+        max_result = max(max_result, total)
+        return
     
-    for i in data:
-        if not visited[i] and len(result) == 0 or result[-1] < i:
-            visited[i] = True
-            result.append(i)
-            dfs(count + 1)
-            
-            visited[i] = False
-            result.pop()
+    if plus > 0:
+        dfs(count + 1, total + num_data[count], plus - 1, minus, mul, div)
+    if minus > 0:
+        dfs(count + 1, total - num_data[count], plus, minus - 1, mul, div)
+    if mul > 0:
+        dfs(count + 1, total * num_data[count], plus, minus, mul - 1, div)
+    if div > 0:
+        dfs(count + 1, int(total / num_data[count]), plus, minus, mul, div - 1)
 
-dfs(0)
+
+dfs(1, num_data[0], op_data[0], op_data[1], op_data[2], op_data[3])
+
+print(max_result)
+print(min_result)
