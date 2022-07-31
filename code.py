626e23f6332467cb824b41a51211dc2700ce6de6
tkyung05 <tkyung6761@gmail.com>
2022-07-31 16:43:33 +0900

    BOJ 9461 파도반 수열 : 간단한 dp 문제

--- code.py
+++ code.py
@@ -3,16 +3,19 @@ input = sys.stdin.readline
 
 T = int(input())
 
-limit = 11
-
 for _ in range(T):
     n = int(input())
+    dp = [0] * (n + 1)
 
-    dp = [0] * (limit + 1)
-    dp[1], dp[2], dp[3] = 1, 2, 4
+    if n == 1 or n == 2 or n == 3:
+        print(1)
+        continue
+
+    dp[1], dp[2], dp[3] = 1, 1, 1
 
     for i in range(4, n + 1):
-        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+        dp[i] = dp[i - 2] + dp[i - 3]
     
     print(dp[n])
-    
+
+
