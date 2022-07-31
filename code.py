a6789fe2df4ff797d7e55189a01cf23e1eaa7231
tkyung05 <tkyung6761@gmail.com>
2022-07-31 17:29:44 +0900

    BOJ 11052 카드 구매하기 : ㅎㅎ DP 실버 1 원큐에 성공!..

--- code.py
+++ code.py
@@ -1,21 +1,21 @@
 import sys
 input = sys.stdin.readline
 
-T = int(input())
+n = int(input())
+price_data = [0] + list(map(int, input().split()))
 
-for _ in range(T):
-    n = int(input())
-    dp = [0] * (n + 1)
+dp = [0] * (n + 1)
+for i in range(1, n + 1): dp[i] = price_data[i]
 
-    if n == 1 or n == 2 or n == 3:
-        print(1)
-        continue
+if n == 1:
+    print(dp[1])
+    exit(0)
 
-    dp[1], dp[2], dp[3] = 1, 1, 1
+dp[2] = max(dp[1] * 2, dp[2])
 
-    for i in range(4, n + 1):
-        dp[i] = dp[i - 2] + dp[i - 3]
-    
-    print(dp[n])
+for i in range(3, n + 1):
+    for j in range(1, i // 2 + 1):
+        dp[i] = max(dp[i], dp[i - j] + price_data[j])
+    price_data[i] = dp[i]
 
-
+print(dp[n])
