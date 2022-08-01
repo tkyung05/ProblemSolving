3dba71d6a6c9c8580a9c8ae6e1234c7775e8207a
tkyung05 <tkyung6761@gmail.com>
2022-08-01 12:38:19 +0900

    BOJ 2579 계단 오르기 : 재밌네 DP

--- code.py
+++ code.py
@@ -2,21 +2,21 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-price_data = [0] + list(map(int, input().split()))
+
+stairs = [0] * (n + 1)
+for i in range(1, n + 1): stairs[i] = int(input())
 
 dp = [0] * (n + 1)
-for i in range(1, n + 1): dp[i] = price_data[i]
 
-if n == 1:
-    print(dp[1])
+if n < 3:
+    print(sum(stairs))
     exit(0)
 
-dp[2] = min(dp[1] * 2, dp[2])
-price_data[2] = dp[2]
+dp[1] = stairs[1]
+dp[2] = stairs[1] + stairs[2]
+dp[3] = max(stairs[1] + stairs[3], stairs[2] + stairs[3])
 
 for i in range(3, n + 1):
-    for j in range(1, i // 2 + 1):
-        dp[i] = min(dp[i], dp[i - j] + price_data[j])
-    price_data[i] = dp[i]
+    dp[i] = max(stairs[i] + dp[i - 3] + stairs[i - 1], stairs[i] + dp[i - 2])
 
 print(dp[n])
