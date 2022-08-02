5200def360d2bdf0a0bec0b9199e5607c17ce293
tkyung05 <tkyung6761@gmail.com>
2022-08-02 13:41:15 +0900

    BOJ 2193 이친수 : 실버지만  DP 14분 컷 오늘 좀 지니어스 하네..음

--- code.py
+++ code.py
@@ -1,24 +1,19 @@
 import sys
 input = sys.stdin.readline
 
-limit = 100001
-div_num = 1000000009
+n = int(input())
 
-T = int(input())
+dp = [1] * (n + 1)
 
-dp = [[0] * 4 for _ in range(limit)]
+if n < 3:
+    print(dp[n])
+    exit(0)
 
-dp[1][0], dp[1][1] = 1, 1
-dp[2][0], dp[2][2] = 1, 1
-dp[3][0], dp[3][1], dp[3][2], dp[3][3] = 3, 1, 1, 1
+dp[3] = 2
 
-for i in range(4, limit):
-    dp[i][1] = dp[i - 1][0] - dp[i - 1][1]
-    dp[i][2] = dp[i - 2][0] - dp[i - 2][2]
-    dp[i][3] = dp[i - 3][0] - dp[i - 3][3]
-        
-    dp[i][0] = (dp[i][1] + dp[i][2] + dp[i][3]) % div_num
+for i in range(4, n + 1):
+    for j in range(1, i - 1):
+        dp[i] += dp[j]
 
-for _ in range(T): 
-    n = int(input())
-    print(dp[n][0])
+print(dp[n])
+
