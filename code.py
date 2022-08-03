594fe472fbb8766b9c80aacae237ea42afb6006f
tkyung05 <tkyung6761@gmail.com>
2022-08-03 15:57:07 +0900

    BOJ 14002 가장 긴 증가하는 부분 수열 4 : 옹 날먹 골드 DP 풀어따~!

--- code.py
+++ code.py
@@ -6,10 +6,25 @@ n = int(input())
 data = list(map(int, input().split()))
 
 dp = [1] * n
+lis = data.copy()
 
 for i in range(n):
     for j in range(i):
         if data[i] > data[j]:
+            before = dp[i]
             dp[i] = max(dp[i], dp[j] + 1)
 
-print(max(dp))
+            if before != dp[i]:
+                lis[i] = j
+
+top_index = dp.index(max(dp))
+
+print(dp[top_index])
+
+result_lis = []
+for _ in range(dp[top_index]):
+    result_lis.append(data[top_index])
+    top_index = lis[top_index]
+result_lis.reverse()
+
+print(' '.join(list(map(str,result_lis))))
