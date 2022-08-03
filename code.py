011788ead57840f35dabcddbe5f6be745b40f1f4
tkyung05 <tkyung6761@gmail.com>
2022-08-03 14:38:54 +0900

    BOJ 11053 가장 긴 증가하는 부분 수열 : LIS 그냥 거의 그리디 + 메모이제이션 문제같네.

--- code.py
+++ code.py
@@ -3,15 +3,13 @@ input = sys.stdin.readline
 
 n = int(input())
 
-dp = [[0] * 10 for _ in range(n + 1)]
+data = list(map(int, input().split()))
 
-for i in range(1, 10): dp[1][i] = 1
- 
-for i in range(2, n + 1):
-    dp[i][0] = dp[i - 1][1]
-    dp[i][9] = dp[i - 1][8]
+dp = [1] * n
 
-    for j in range(1, 9):
-        dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1]
-    
-print(sum(dp[n]) % 1000000000)
+for i in range(n):
+    for j in range(i):
+        if data[i] > data[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(max(dp))
