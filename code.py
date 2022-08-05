a15d0e2200eb2edf27a04ef9446cace27639b274
tkyung05 <tkyung6761@gmail.com>
2022-08-05 14:27:24 +0900

    BOJ 1309 동물원 : 타일링과 비슷한 DP 문제

--- code.py
+++ code.py
@@ -2,12 +2,18 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-sequence = list(map(int, input().split()))
 
-dp = [0] * n
-dp[0] = sequence[0]
+dp = [[0] * 3 for _ in range(100000 + 1)]
 
-for i in range(1, n):
-    dp[i] = max(dp[i - 1] + sequence[i], sequence[i])
+dp[1][0], dp[1][1], dp[1][2] = 3, 1, 1
+dp[2][0], dp[2][1], dp[2][2] = 7, 2, 2
 
-print(max(dp))
+div_num = 9901
+
+for i in range(3, n + 1):
+    dp[i][1] = (dp[i - 1][0] - dp[i - 1][1]) % div_num
+    dp[i][2] = (dp[i - 1][0] - dp[i - 1][2]) % div_num
+
+    dp[i][0] = (dp[i - 1][0] + dp[i][1] + dp[i][2]) % div_num
+
+print(dp[n][0])
