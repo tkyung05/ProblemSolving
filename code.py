cbff8b4f5667d9166c2057c75431e012c9c8ac72
tkyung05 <tkyung6761@gmail.com>
2022-08-05 15:00:45 +0900

    BOJ 11057 오르막 수 : 웰 논 DP

--- code.py
+++ code.py
@@ -3,17 +3,15 @@ input = sys.stdin.readline
 
 n = int(input())
 
-dp = [[0] * 3 for _ in range(100000 + 1)]
+dp = [[0] * 10 for _ in range(n + 1)]
 
-dp[1][0], dp[1][1], dp[1][2] = 3, 1, 1
-dp[2][0], dp[2][1], dp[2][2] = 7, 2, 2
+for i in range(10): dp[1][i] = 1 
 
-div_num = 9901
+div_num = 10007
 
-for i in range(3, n + 1):
-    dp[i][1] = (dp[i - 1][0] - dp[i - 1][1]) % div_num
-    dp[i][2] = (dp[i - 1][0] - dp[i - 1][2]) % div_num
+for i in range(2, n + 1):
+    for j in range(10):
+        for k in range(j, 10):
+            dp[i][j] += dp[i - 1][k] 
 
-    dp[i][0] = (dp[i - 1][0] + dp[i][1] + dp[i][2]) % div_num
-
-print(dp[n][0])
+print(sum(dp[n]) % div_num)
