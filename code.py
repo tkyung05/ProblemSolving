e0fe367261a712a2dd308850e2b5be9b828ede36
tkyung05 <tkyung6761@gmail.com>
2022-08-06 14:29:37 +0900

    BOJ 1003 피보나치 함수 : ez

--- code.py
+++ code.py
@@ -1,25 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
+T = int(input())
 
-dp = [0]
-for _ in range(n): dp.append(list(map(int, input().split())))
+limit = 41
+dp = [[0] * 2 for _ in range(limit)]
 
-if n == 1:
-    print(dp[1][0])
-    exit(0)
+dp[0][0], dp[0][1] = 1, 0
+dp[1][0], dp[1][1] = 0, 1
 
-dp[2][0], dp[2][1] = dp[2][0] + dp[1][0], dp[2][1] + dp[1][0]
+for i in range(2, limit):
+    dp[i][0] = dp[i - 1][0] + dp[i - 2][0]
+    dp[i][1] = dp[i - 1][1] + dp[i - 2][1]
 
-for i in range(3, n + 1):
-    for j in range(i):
-        if j == 0:
-            dp[i][0] += dp[i - 1][0]
-        elif j == i - 1:
-            dp[i][j] += dp[i - 1][j - 1]
-
-        else:
-            dp[i][j] += max(dp[i - 1][j], dp[i - 1][j - 1])
-
-print(max(dp[-1]))
+for _ in range(T): 
+    n = int(input())
+    print(dp[n][0], dp[n][1])
