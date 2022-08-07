8ce9bdd44ed667b09f44cd3b20fa490db7e26f42
tkyung05 <tkyung6761@gmail.com>
2022-08-07 12:38:29 +0900

    BOJ 1699 제곱수의 합 : 1 ~ N 제곱근 까지 제곱수를 구해서 최소 항을 구하는 DP 문제

--- code.py
+++ code.py
@@ -1,18 +1,13 @@
 import sys
 input = sys.stdin.readline
 
-T = int(input())
+n = int(input())
 
-limit = 41
-dp = [[0] * 2 for _ in range(limit)]
+dp = [0] * (n + 1)
 
-dp[0][0], dp[0][1] = 1, 0
-dp[1][0], dp[1][1] = 0, 1
-
-for i in range(2, limit):
-    dp[i][0] = dp[i - 1][0] + dp[i - 2][0]
-    dp[i][1] = dp[i - 1][1] + dp[i - 2][1]
-
-for _ in range(T): 
-    n = int(input())
-    print(dp[n][0], dp[n][1])
+for i in range(1, n + 1):
+    dp[i] = i
+    for j in range(1, int(i ** 0.5) + 1):
+       dp[i] = min(dp[i], dp[i - j * j] + 1) 
+    
+print(dp[n])
