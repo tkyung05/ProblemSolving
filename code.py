aa9aa47401865611254408d6d6a9ce79a76f3bde
tkyung05 <tkyung6761@gmail.com>
2022-08-09 15:12:31 +0900

    BOJ 18429 근손실 : 백트랙킹 실버 정복해야겠다.

--- code.py
+++ code.py
@@ -1,39 +1,43 @@
 import sys
 input = sys.stdin.readline
 
-k = int(input())
-data = list(map(str, input().split()))
+n, k = map(int, input().split())
+kit = list(map(int, input().split()))
+
+result = 0
 
 sequence = []
-visited = [False] * 10
+visited = [False] * n
 
-result = []
+now_weight = 500
 
 def dfs(depth):
-    
-    if depth > 1:
-        if data[depth - 2] == '<':
-            if not sequence[-2] < sequence[-1]:
-                return 
-        else:
-            if not sequence[-2] > sequence[-1]:
-                return
+    global result, now_weight
 
-    if depth == k + 1:
-        result.append(list(map(str, sequence)))
+    if depth != 0:
+        now_weight -= k
+        now_weight += sequence[-1]
+        
+        if now_weight < 500:
+            return 
+
+    if depth == n:
+        result += 1
         return
 
-    for i in range(10):
+    for i in range(n):
         if not visited[i]:
             visited[i] = True
-            sequence.append(i)
+            sequence.append(kit[i])
 
             dfs(depth + 1)
+
+            now_weight += k
+            now_weight -= kit[i]
 
             visited[i] = False
             sequence.pop()
 
-dfs(0)   
+dfs(0)
 
-print(''.join(result[-1]))
-print(''.join(result[0]))
+print(result)
