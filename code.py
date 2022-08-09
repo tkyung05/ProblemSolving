ee547b0d1fdce420843022d4646212b1c01af8e7
tkyung05 <tkyung6761@gmail.com>
2022-08-09 13:23:58 +0900

    BOJ 10819 차이를 최대로 : 백트랙킹 연습

--- code.py
+++ code.py
@@ -2,23 +2,34 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
+sequence = list(map(int, input().split()))
 
-result = []
 visited = [False] * n
+stack = []
+
+max_total = -1e9
+
 
 def dfs(depth):
-    if depth == n:
-        print(' '.join(list(map(str, result))))
-        return
+    global max_total
 
+    if depth == n:
+        result = 0
+        for i in range(1, n):
+            result += abs(stack[i - 1] - stack[i])
+        max_total = max(result, max_total)
+        return
+        
     for i in range(n):
         if not visited[i]:
             visited[i] = True
-            result.append(i + 1)
-            
+            stack.append(sequence[i])
+
             dfs(depth + 1)
             
             visited[i] = False
-            result.pop()
+            stack.pop()
     
 dfs(0)
+
+print(max_total)
