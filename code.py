d69da4f4f71d4310f7454804c0a318e1afedc654
tkyung05 <tkyung6761@gmail.com>
2022-08-10 23:39:48 +0900

    BOJ 7490 0 만들기 : 백트랙킹 골드 풀었따!!

--- code.py
+++ code.py
@@ -1,66 +1,45 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-graph = [list(map(str, input().split())) for _ in range(n)]
+T = int(input())
 
-visited = [[False] * n for _ in range(n)]
+operator = [' ', '+', '-']
 sequence = []
-teacher = []
 
-for i in range(n):
-    for j in range(n):
-        if graph[i][j] == 'T':
-            teacher.append((i, j))
-            visited[i][j] = True
-        elif graph[i][j] == 'S':
-            visited[i][j] = True
+def dfs(depth):
 
-dy = [1, -1, 0, 0]
-dx = [0, 0, -1, 1]
+    if depth == n - 1:
+        total = ['1']
+        result = ['1']
+        
+        for i in range(n - 1):
+            now_num = str(i + 2)
 
-def check():
-    for y, x in teacher:
-        for i in range(4):
+            if sequence[i] == ' ':
+                total[-1] = str(total[-1] + now_num)
+                result.append(' ')
+            elif sequence[i] == '+':
+                total.append(now_num)
+                result.append('+')
+            elif sequence[i] == '-':
+                total.append(str('-' + now_num)) 
+                result.append('-')
             
-            for j in range(n):
-                if i == 0 or i == 1:
-                    ny = dy[i] + (j * dy[i])
-                    nx = 0
-                elif i == 2 or i == 3:
-                    nx = dx[i] + (j * dx[i])
-                    ny = 0
-                
-                cy, cx = y + ny, x + nx
-                if cy < 0 or cy >= n or cx < 0 or cx >= n:
-                    break
+            result.append(now_num)
 
-                if graph[cy][cx] == 'O':
-                    break
-                if graph[cy][cx] == 'S':
-                    return False
-    return True
+        if sum(list(map(int, total))) == 0:
+            print(''.join(result))
+            
+        return 
+
+    for i in operator:
+        sequence.append(i)
+        dfs(depth + 1)
+        sequence.pop()
 
 
-def dfs(depth, idx):
+for _ in range(T):
+    n = int(input())
 
-    if depth == 3:
-        if check():
-            print('YES')
-            exit(0)
-        return
-    
-    for i in range(idx, n):
-        for j in range(n):
-            if not visited[i][j]:
-                visited[i][j] = True
-                graph[i][j] = 'O'
-
-                dfs(depth + 1, i)
-
-                visited[i][j] = False
-                graph[i][j] = 'X'
-
-dfs(0, 0)
-
-print('NO')
+    dfs(0)
+    print()
