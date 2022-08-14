a4252f793341cc8d24ce29aa15b55d4724665aa9
tkyung05 <tkyung6761@gmail.com>
2022-08-14 13:19:35 +0900

    BOJ 2493 탑 : 스택 활용 문제!~

--- code.py
+++ code.py
@@ -1,39 +1,16 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
-T = int(input())
+n = int(input())
+tower = list(map(int, input().split()))
 
-for _ in range(T):
-    cmd = list(map(str, input().strip('\n')))
-    n = int(input())
-    data = deque(list(map(str, input().split(','))))
+result = [0] * n
+stack = []
 
-    if n > 0: 
-        data[0] = data[0].strip('[')
-        data[-1] = data[-1].strip(']\n')
-    else:
-        data = deque()
-    
-    direction = True # True = 오른쪽, False = 왼쪽
-    bug = False
+for i in range(n):
+    while stack and tower[stack[-1]] <= tower[(n-1) - i]:
+        result[stack.pop()] = n - i
 
-    for cm in cmd:
-        if cm == 'R':
-            if direction: direction = False
-            else: direction = True
-        
-        elif cm == 'D':
-            if len(data) == 0:
-                print('error')
-                bug = True
-                break
+    stack.append((n-1) - i)
 
-            if direction: data.popleft()
-            else: data.pop()
-    
-    if bug: continue
-
-    if not direction: data.reverse()
-    
-    print('[' + ','.join(data) + ']')
+print(*result)
