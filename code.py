219141c529e65a960d0b2af1910f4005c96d261a
tkyung05 <tkyung6761@gmail.com>
2022-08-16 08:57:52 +0900

    BOJ 15661 링크와 스타트 : 기초 백트랙킹 문제.

--- code.py
+++ code.py
@@ -1,30 +1,48 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
-T = int(input())
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
 
-for _ in range(T):
-    n, m = map(int, input().split())
-    queue = deque(list(map(int, input().split())))
+comb = []
+visited = [False] * n
+result = int(1e9)
 
-    for i in range(n): queue[i] = (i, queue[i])
-    
-    count = 1
-    while True:
-        idx, num = queue.popleft()
-            
-        bug = False
-        for i in range(len(queue)):
-            if num < queue[i][1]:
-                bug = True
-                break
+def dfs(depth, idx, team_size):
+    global result
+
+    if depth == team_size:
+        link, start = 0, 0
+
+        start_list = []
+        for i in range(n):
+            if not visited[i]:
+                start_list.append(i)
         
-        if bug: 
-            queue.append((idx, num))
-        else:
-            if idx == m: 
-                print(count)
-                break
-            count += 1
-    
+        for i in range(team_size):
+            for j in range(i, team_size):
+                link += graph[comb[i]][comb[j]] + graph[comb[j]][comb[i]]
+        
+        for i in range(len(start_list)):
+            for j in range(i, len(start_list)):
+                start += graph[start_list[i]][start_list[j]] + graph[start_list[j]][start_list[i]]
+        
+        result = min(result, abs(link - start))
+        return
+
+    for i in range(idx, n):
+        visited[i] = True
+        comb.append(i)
+
+        dfs(depth + 1, i + 1, team_size)
+        
+        visited[i] = False
+        comb.pop()
+
+
+for i in range(2, int(n//2) + 1):
+    dfs(0, 0, i)
+    if result == 0:
+        break
+
+print(result)
