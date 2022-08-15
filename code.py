ec055bb514a3edec6d36de930c43f2443b380087
tkyung05 <tkyung6761@gmail.com>
2022-08-15 11:13:22 +0900

    BOJ 18290 NM과 K (1) : 백트랙킹

--- code.py
+++ code.py
@@ -1,16 +1,42 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-tower = list(map(int, input().split()))
+n, m, k = map(int, input().split()) 
+graph = [list(map(int, input().split())) for _ in range(n)]
 
-result = [0] * n
-stack = []
+visited = [[False] * m for _ in range(n)]
+result = int(-1e9)
 
-for i in range(n):
-    while stack and tower[stack[-1]] <= tower[(n-1) - i]:
-        result[stack.pop()] = n - i
+dy = [-1, 1, 0, 0]
+dx = [0, 0, -1, 1]
 
-    stack.append((n-1) - i)
+def dfs(depth, total, py, px):
+    global result
 
-print(*result)
+    if depth == k:
+        result = max(result, total)
+        return
+
+    for i in range(py, n):
+        for j in range(px if i == py else 0, m):
+
+            if not visited[i][j]:    
+                visit_memo = []
+                visited[i][j] = True
+
+                for d in range(4):
+                    ny, nx = i + dy[d], j + dx[d]
+                    if ny < 0 or ny >= n or nx < 0 or nx >= m:
+                        continue
+                    if not visited[ny][nx]: 
+                        visited[ny][nx] = True
+                        visit_memo.append((ny, nx))
+                
+                dfs(depth + 1, total + graph[i][j], i, j)
+
+                visited[i][j] = False
+                for y, x in visit_memo:
+                    visited[y][x] = False
+
+dfs(0, 0, 0, 0)
+print(result)
