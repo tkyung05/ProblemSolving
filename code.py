141a314bfc25bd47a1650c3a6170cbcf1ca8f925
tkyung05 <tkyung6761@gmail.com>
2022-08-17 14:00:54 +0900

    BOJ 19941 햄버거 분배 : 그리디하게 푸는 문제~ ><.

--- code.py
+++ code.py
@@ -1,34 +1,21 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
-n = int(input())
+n, k = map(int, input().split())
+graph = list(map(str, input().strip('\n')))
 
-emoji = [[0 for _ in range(n)] for _ in range(n + 1)]
+visited = [False] * n
+result = 0
 
-def bfs():
-    q = deque([(1, 0)])
-    emoji[1][0] = 1
-     
-    while q:
-        screen, clip = q.popleft()
+for i in range(n):
+    if graph[i] == 'P':
+        start = i - k if i - k > 0 else 0
+        end = i + k if i + k <= n - 1 else n - 1
         
-        if screen == n:
-            result = int(1e9)
-            for i in emoji[n]:
-                if i != 0: result = min(result, i)
-            return result - 1
+        for j in range(start, end + 1):
+            if graph[j] == 'H' and not visited[j]:
+                visited[j] = True
+                result += 1
+                break
 
-        if emoji[screen][screen] == 0:
-            emoji[screen][screen] = emoji[screen][clip] + 1
-            q.append((screen, screen))
-        
-        if clip != 0 and (screen + clip) <= n and emoji[screen + clip][clip] == 0:
-            emoji[screen + clip][clip] = emoji[screen][clip] + 1
-            q.append((screen + clip, clip))
-        
-        if (screen - 1) > 1 and emoji[screen - 1][clip] == 0:
-            emoji[screen - 1][clip] = emoji[screen][clip] + 1
-            q.append((screen - 1, clip))
-        
-print(bfs())
+print(result)
