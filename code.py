c6d018930b6a8baed04e2486a0a495479500422f
tkyung05 <tkyung6761@gmail.com>
2022-08-20 15:49:34 +0900

    BOJ 2559 수열 : 쉬운 구간합 문제.

--- code.py
+++ code.py
@@ -1,38 +1,12 @@
 import sys
 input = sys.stdin.readline
 
-a, b = map(str, input().split())
+n, k = map(int, input().split())
+data = list(map(int, input().split()))
 
-B = int(b)
+for i in range(1, n): data[i] += data[i - 1]
 
-data = list(map(int, a))
-sequence = []
-visited = [False] * len(data)
+result = data[k - 1]
+for i in range(n - k): result = max(result, data[k + i] - data[i])
 
-result = -1
-
-def dfs(depth):
-    global result
-    
-    if depth == len(data):
-        total = int(''.join(sequence))
-
-        if total < B:
-            result = max(result, total)
-        return
-
-    for i in range(len(data)):
-        if not visited[i]:
-            if depth == 0 and data[i] == 0:
-                continue
-
-            visited[i] = True
-            sequence.append(str(data[i]))
-
-            dfs(depth + 1)
-
-            visited[i] = False
-            sequence.pop()
-
-dfs(0)
 print(result)
