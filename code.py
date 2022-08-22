8bb0b5e5c9aa7a319a67fd5d0e79ca768728b5bb
tkyung05 <tkyung6761@gmail.com>
2022-08-22 16:04:34 +0900

    BOJ 3190 뱀 : 큐 활용 빡구현 문제.

--- code.py
+++ code.py
@@ -1,18 +1,66 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
 n = int(input())
-work = [list(map(int, input().split())) for _ in range(n)]
+k = int(input())
 
-dp = [0] * (n + 1)
+graph = [[0] * n for _ in range(n)]
 
-for day in range(n-1, -1, -1):
-  time = work[day][0]
+dy = [0, 1, 0, -1]
+dx = [1, 0, -1, 0]
+
+for _ in range(k):
+  y, x = map(int, input().split())
+  graph[y - 1][x - 1] = 2
+
+m = int(input())
+dir_data = {}
+for _ in range(m):
+  sec, dir = input().split()
+  dir_data[int(sec)] = dir
+
+# 뱀 초기 값 세팅
+snake_body = deque([(0, 0)])
+snake_y, snake_x = 0, 0
+snake_direct = 0
+graph[0][0] = 1
+
+def turn_snake(dir):
+  global snake_direct
+
+  if dir == 'L':
+    snake_direct -= 1
+  elif dir == 'D':
+    snake_direct += 1
+  snake_direct = (snake_direct % 4)
+
+count = 0
+
+while True:
+  snake_y += dy[snake_direct]
+  snake_x += dx[snake_direct]
+
+  count += 1
+
+  if snake_y < 0 or snake_y >= n or snake_x < 0 or snake_x >= n:
+    break
   
-  if day + time <= n:
-    price = work[day][1]  
-    dp[day] = max(dp[day + 1], dp[day + time] + price)
+  elif graph[snake_y][snake_x] == 2:
+    graph[snake_y][snake_x] = 1
+    snake_body.append((snake_y, snake_x))
+    if count in dir_data:
+      turn_snake(dir_data[count])
+  
+  elif graph[snake_y][snake_x] == 0:
+    graph[snake_y][snake_x] = 1
+    snake_body.append((snake_y, snake_x))
+    cy, cx = snake_body.popleft()
+    graph[cy][cx] = 0
+    if count in dir_data:
+      turn_snake(dir_data[count])
+  
   else:
-    dp[day] = dp[day + 1]
+    break
 
-print(dp[0])
+print(count)
