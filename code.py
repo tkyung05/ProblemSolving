f51c3e16af7faf22f4cbc6a0195e15c053bfb738
tkyung05 <tkyung6761@gmail.com>
2022-08-23 08:14:32 +0900

    BOJ 5568 카드 놓기 : 쉬운 블포, 백트 문제.

--- code.py
+++ code.py
@@ -1,66 +1,32 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
 
 n = int(input())
 k = int(input())
 
-graph = [[0] * n for _ in range(n)]
+card = [int(input()) for _ in range(n)]
 
-dy = [0, 1, 0, -1]
-dx = [1, 0, -1, 0]
+combi = []
+result = {}
+visited = [False] * n
 
-for _ in range(k):
-  y, x = map(int, input().split())
-  graph[y - 1][x - 1] = 2
+def dfs(depth):
+  if depth == k:
+    num = ''
+    for i in combi:
+      num += str(card[i])
+    result[num] = 1
+    return
 
-m = int(input())
-dir_data = {}
-for _ in range(m):
-  sec, dir = input().split()
-  dir_data[int(sec)] = dir
+  for i in range(n):
+    if not visited[i]:
+      visited[i] = True
+      combi.append(i)
+      
+      dfs(depth + 1)
+      
+      visited[i] = False
+      combi.pop()
 
-# 뱀 초기 값 세팅
-snake_body = deque([(0, 0)])
-snake_y, snake_x = 0, 0
-snake_direct = 0
-graph[0][0] = 1
-
-def turn_snake(dir):
-  global snake_direct
-
-  if dir == 'L':
-    snake_direct -= 1
-  elif dir == 'D':
-    snake_direct += 1
-  snake_direct = (snake_direct % 4)
-
-count = 0
-
-while True:
-  snake_y += dy[snake_direct]
-  snake_x += dx[snake_direct]
-
-  count += 1
-
-  if snake_y < 0 or snake_y >= n or snake_x < 0 or snake_x >= n:
-    break
-  
-  elif graph[snake_y][snake_x] == 2:
-    graph[snake_y][snake_x] = 1
-    snake_body.append((snake_y, snake_x))
-    if count in dir_data:
-      turn_snake(dir_data[count])
-  
-  elif graph[snake_y][snake_x] == 0:
-    graph[snake_y][snake_x] = 1
-    snake_body.append((snake_y, snake_x))
-    cy, cx = snake_body.popleft()
-    graph[cy][cx] = 0
-    if count in dir_data:
-      turn_snake(dir_data[count])
-  
-  else:
-    break
-
-print(count)
+dfs(0)
+print(len(result.values()))
