9ef65ab5aaa4227ac699a1ac757ba8e120aebd51
tkyung05 <tkyung6761@gmail.com>
2022-08-24 23:12:50 +0900

    BOJ 14495 피보나치 비스무리한 수열 : 대충  날먹 dp로 스트릭 채우기.

--- code.py
+++ code.py
@@ -1,14 +1,12 @@
-def solution(triangle):
-    answer = 0
-    
-    for i in range(1, len(triangle)):
-        for j in range(len(triangle[i])):
-            if j == 0:
-                triangle[i][0] += triangle[i - 1][0]
-            elif j == len(triangle[i]) - 1:
-                triangle[i][-1] += triangle[i - 1][-1] 
-            else:
-                triangle[i][j] += max(triangle[i - 1][j], triangle[i - 1][j - 1])
-                
-    answer = max(triangle[-1])
-    return answer
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [0] * 117
+dp[1], dp[2], dp[3] = 1, 1, 1
+
+for i in range(4, n+1):
+  dp[i] = dp[i - 1] + dp[i - 3]
+
+print(dp[n])
