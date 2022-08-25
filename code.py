73f8a0ebfb753d3ab3c32f8f5349dd73b2ee4549
tkyung05 <tkyung6761@gmail.com>
2022-08-25 15:04:41 +0900

    프로그래머스 디스크 컨트롤러 level 3 : 풀이 성공!.. 근데 코드가 너무 더럽다 다음부터 좀 더 신경 써야겠다.

--- code.py
+++ code.py
@@ -1,12 +1,39 @@
-import sys
-input = sys.stdin.readline
+import heapq
 
-n = int(input())
+def solution(jobs):
+    answer = []
+    disk_count = len(jobs)
+    
+    heapq.heapify(jobs)
+  
+    total_time = 0
 
-dp = [0] * 117
-dp[1], dp[2], dp[3] = 1, 1, 1
+    while jobs:
+      temp_disk = []
+      only_one = False
 
-for i in range(4, n+1):
-  dp[i] = dp[i - 1] + dp[i - 3]
+      # 마지막으로 처리한 시간보다 작거나 같은 디스크를 넣어줌
+      while jobs and jobs[0][0] <= total_time:
+        ask_time, work_time = heapq.heappop(jobs)
+        heapq.heappush(temp_disk, (work_time, ask_time))
 
-print(dp[n])
+      # 요청 시간이 total 보다 큰 경우
+      if not temp_disk:
+        ask_time, work_time = heapq.heappop(jobs)
+        heapq.heappush(temp_disk, (work_time, ask_time))
+        only_one = True
+
+      now_work_time, now_ask_time = heapq.heappop(temp_disk)
+      
+      if only_one:
+        answer.append(now_work_time)
+        total_time = now_ask_time + now_work_time
+      else:
+        answer.append((total_time - now_ask_time) + now_work_time)
+        total_time += now_work_time
+
+      while temp_disk:
+        work, ask = heapq.heappop(temp_disk)
+        heapq.heappush(jobs, [ask, work])
+
+    return sum(answer) // disk_count
