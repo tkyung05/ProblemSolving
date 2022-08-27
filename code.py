3975f1916f5abb59b6b5938294a111895a4248fb
tkyung05 <tkyung6761@gmail.com>
2022-08-27 17:14:54 +0900

    PMS level 2 H-index : 정렬 문제, 나름 식 세워서 잘 짠 것 같음. ㅎㅎ

--- code.py
+++ code.py
@@ -1,54 +1,11 @@
-import heapq
-
-def solution(operations):
-    answer = [0] * 2
+def solution(citations):
+    answer = 0
+    citations.sort()
     
-    min_heap = []
-    max_heap = []
-    visited = [False] * 1000000
+    for h in range(len(citations)):
+        for i in range(len(citations)):
+            if h + 1 <= citations[i] and len(citations) - i >= h + 1 and i <= h + 1:
+                answer = h + 1
+                break
     
-    for i in range(len(operations)):
-        cmd, num = operations[i].split()
-        
-        if cmd == 'I':
-            heapq.heappush(min_heap, (int(num), i))
-            heapq.heappush(max_heap, (-1 * int(num), i))
-            
-            visited[i] = True
-        
-        else:
-            if num == '-1':
-                while min_heap and not visited[min_heap[0][1]]:
-                    heapq.heappop(min_heap)
-                
-                if min_heap:
-                    number, idx = heapq.heappop(min_heap)
-                    visited[idx] = False
-            
-            if num == '1':
-                while max_heap and not visited[max_heap[0][1]]:
-                    heapq.heappop(max_heap)
-                
-                if max_heap:
-                    number, idx = heapq.heappop(max_heap)
-                    visited[idx] = False
-    
-    while min_heap and not visited[min_heap[0][1]]:
-        heapq.heappop(min_heap)
-    
-    while max_heap and not visited[max_heap[0][1]]:
-        heapq.heappop(max_heap)
-    
-    
-    if len(min_heap) == 0 or len(max_heap) == 0:
-        return answer
-    else:
-        answer[0] = heapq.heappop(max_heap)[0] * -1
-        answer[1] = heapq.heappop(min_heap)[0]
-        return answer
-
-
-
-
-
-
+    return answer
