878ebf2fc64299bb6ced996f40cc89b889ecc521
tkyung05 <tkyung6761@gmail.com>
2022-08-31 09:23:21 +0900

    PMS level 1 신규 아이디 추천 : 2021 카카오 블라인드 채용 : ㄹㅇ 스파게티 코드인데 다른 사람들은 완전 간단하게 풀었네. 나도 클린코드  연습해야지.

--- code.py
+++ code.py
@@ -1,41 +1,36 @@
-def solution(numbers, hand):
-    answer = []
+def solution(new_id):
+    answer = ''
+    not_possible = {'/': 1, '~': 1, '!': 1, '@': 1, '#': 1, '$': 1, '%': 1, '^': 1, '&': 1, '*': 1, '(': 1, ')': 1, '=': 1, '+': 1, '[': 1, ']': 1, '{': 1, '}': 1, ':': 1, '?': 1, ',': 1, '<': 1, '>': 1}
     
-    left_nums = {'1': (0, 0), '4': (1, 0), '7': (2, 0)}
-    right_nums = {'3': (0, 2), '6': (1, 2), '9': (2, 2)}
-    other_nums = {'2' : (0, 1), '5' : (1, 1), '8' : (2, 1), '0' : (3, 1)}
+    one_step = new_id.lower()
+    two_step = ''
+    for i in one_step:
+        if i in not_possible: continue
+        else: two_step += i
     
-    now_left_pos = (3, 0)
-    now_right_pos = (3, 2)
+    three_step = ''
+    for i in two_step:
+        if len(list(three_step)) != 0 and three_step[-1] == '.' and i == '.': continue
+        else: three_step += i
     
-    for num in numbers:
-        if str(num) in left_nums:
-            now_left_pos = left_nums[str(num)]
-            answer.append('L')
-        elif str(num) in right_nums:
-            now_right_pos = right_nums[str(num)]
-            answer.append('R')
-        else:
-            dy, dx = other_nums[str(num)]
-            l_y, l_x = now_left_pos
-            r_y, r_x = now_right_pos
+    if len(list(three_step)) != 0:
+        if three_step[0] == '.':
+            three_step = three_step[1:]
+        if len(list(three_step)) != 0 and three_step[-1] == '.':
+            three_step = three_step[:len(list(three_step)) - 1]
+
+    if len(list(three_step)) == 0:
+        three_step = 'a'
+    
+    if len(list(three_step)) >= 16:
+        three_step = three_step[:15] 
+        if three_step[-1] == '.':
+            three_step = three_step[:14]
             
-            left_gap = abs(dy - l_y) + abs(dx - l_x)
-            right_gap = abs(dy - r_y) + abs(dx - r_x)
-            
-            if left_gap < right_gap:
-                answer.append('L')
-                now_left_pos = (dy, dx)
-            elif left_gap > right_gap:
-                answer.append('R')
-                now_right_pos = (dy, dx)
-            elif left_gap == right_gap:
-                if hand == 'left':
-                    answer.append('L')
-                    now_left_pos = (dy, dx)
-                else:
-                    answer.append('R')
-                    now_right_pos = (dy, dx)
+    if len(list(three_step)) <= 2:
+        three_step += three_step[-1] * (3 - len(list(three_step)))
         
-        
-    return ''.join(answer)
+    return three_step
+
+
+
