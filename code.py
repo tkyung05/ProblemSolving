c0e14d3a16688f638fb115e8b2aebf325cb3f469
tkyung05 <tkyung6761@gmail.com>
2022-08-31 23:27:02 +0900

    PMS level 2 메뉴 리뉴얼 : 2021 카카오 블라인드 채용 해시, itertools로 조합 만들어 푼 문제. 코드가 솔직히 깔끔하진 않아서 앞으로 더 발전 해야겠다.

--- code.py
+++ code.py
@@ -1,39 +1,31 @@
-def solution(m, n, data):
-    answer = 0
-    board = [list(map(str, i)) for i in data]
-    
-    while True:
-        pop_count = 0
-        visited = [[0] * n for _ in range(m)]
-        
-        # 블록 터트리기
-        for i in range(m - 1):
-            for j in range(n - 1):
-                if board[i][j] == 'EMPTY': continue
-                if board[i][j] == board[i + 1][j] == board[i][j + 1] == board[i + 1][j + 1]:
-                    visited[i][j], visited[i + 1][j], visited[i + 1][j + 1], visited[i][j + 1] = 1, 1, 1, 1           
-        
-        # 터진 블록 개수 카운드
-        for i in range(m):
-            for j in range(n):
-                if visited[i][j] == 1:
-                    pop_count += 1
-                    board[i][j] = 'EMPTY'
-    
-        # 종료 조건
-        if pop_count == 0:
-            break
-        
-        # 블록 떨어지는 기능
-        for i in range(n):
-            for j in range(m - 1, 0, -1):
-                if board[j][i] == 'EMPTY':
-                    for k in range(j - 1, -1, -1):
-                        if board[k][i] != 'EMPTY':
-                            board[j][i] = board[k][i]
-                            board[k][i] = 'EMPTY'
-                            break
-        
-        answer += pop_count 
-                    
-    return answer
+from itertools import *
+
+def solution(orders, course):
+    answer = []
+
+    max_of_size = {}
+    possible_menu = {}
+
+    for o in orders:
+        order = sorted(list(map(str, o)))
+        for size in range(2, len(order) + 1):
+            for com in combinations(order, size):
+                menu = ''.join(list(com))
+                if menu in possible_menu:
+                    possible_menu[menu][0] += 1
+                else:
+                    possible_menu[menu] = [1, size] 
+
+                if size in max_of_size:
+                    max_of_size[size] = max(max_of_size[size], possible_menu[menu][0])
+                else:
+                    max_of_size[size] = possible_menu[menu][0]
+
+    for num in course:
+        if num not in max_of_size or max_of_size[num] == 1:
+            continue
+        for menu in possible_menu:
+            if possible_menu[menu][1] == num and possible_menu[menu][0] == max_of_size[num]:
+                answer.append(menu) 
+  
+    return sorted(answer)
