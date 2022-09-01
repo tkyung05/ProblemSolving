d897cf72b232885b4d6079ee81914f25f174f1d6
tkyung05 <tkyung6761@gmail.com>
2022-09-01 10:51:59 +0900

    PMS level 2 짝지어 제거하기 : 2017 팁스타운 ㄹㅇ 간단한 스택 문제 코드 10줄 컷.

--- code.py
+++ code.py
@@ -1,31 +1,10 @@
-from itertools import *
-
-def solution(orders, course):
-    answer = []
-
-    max_of_size = {}
-    possible_menu = {}
-
-    for o in orders:
-        order = sorted(list(map(str, o)))
-        for size in range(2, len(order) + 1):
-            for com in combinations(order, size):
-                menu = ''.join(list(com))
-                if menu in possible_menu:
-                    possible_menu[menu][0] += 1
-                else:
-                    possible_menu[menu] = [1, size] 
-
-                if size in max_of_size:
-                    max_of_size[size] = max(max_of_size[size], possible_menu[menu][0])
-                else:
-                    max_of_size[size] = possible_menu[menu][0]
-
-    for num in course:
-        if num not in max_of_size or max_of_size[num] == 1:
-            continue
-        for menu in possible_menu:
-            if possible_menu[menu][1] == num and possible_menu[menu][0] == max_of_size[num]:
-                answer.append(menu) 
-  
-    return sorted(answer)
+def solution(s):
+    result = []
+    
+    for alpha in list(s):
+        if result and result[-1] == alpha:
+            result.pop()
+        else:
+            result.append(alpha)
+    
+    return 0 if result else 1
