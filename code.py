b815a66b27c55b00f9fac32ee6f983d8af105d9f
tkyung05 <tkyung6761@gmail.com>
2022-09-01 10:54:01 +0900

    PMS level 2 문자열 압축 : 2020 카카오 블라인드 채용 완탐, 문자열 지문을 잘 못 읽어서 시간 조금 날렸던 문제.

--- code.py
+++ code.py
@@ -1,10 +1,30 @@
 def solution(s):
-    result = []
+    answer = []
+    if len(s) == 1: return 1
     
-    for alpha in list(s):
-        if result and result[-1] == alpha:
-            result.pop()
+    for cut_size in range(1, len(s) // 2 + 1):
+        overlap_count = 1
+        result = ''
+        prev_str = s[:cut_size]
+        
+        
+        for start in range(cut_size, len(s), cut_size):
+            if prev_str == s[start : start + cut_size]:
+                overlap_count += 1
+            else:
+                if overlap_count > 1:
+                    result += str(overlap_count) + prev_str
+                else:
+                    result += prev_str
+                    
+                prev_str = s[start : start + cut_size]
+                overlap_count = 1
+                
+        if overlap_count > 1:
+            result += str(overlap_count) + prev_str
         else:
-            result.append(alpha)
-    
-    return 0 if result else 1
+            result += prev_str
+            
+        answer.append(len(result))
+        
+    return min(answer)
