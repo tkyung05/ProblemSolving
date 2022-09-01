6d59ef42f599058afbbbc5c8990554d5ff92eec8
tkyung05 <tkyung6761@gmail.com>
2022-09-01 14:26:13 +0900

    PMS level 2 수식 최대화 : 2020 카카오 인턴십 완탐, 스택, 구현 문제 꽤 까다로운.

--- code.py
+++ code.py
@@ -1,30 +1,50 @@
-def solution(s):
+from itertools import *
+
+def solution(exp):
     answer = []
-    if len(s) == 1: return 1
+    oper_data = []
+    expression = [ex for ex in exp]
+    og_expression = []
     
-    for cut_size in range(1, len(s) // 2 + 1):
-        overlap_count = 1
-        result = ''
-        prev_str = s[:cut_size]
+    for i in range(len(expression)):
+        if expression[i] == '*' or expression[i] == '-' or expression[i] == '+':
+            oper_data.append(expression[i])
+            expression[i] = '.'
+    
+    exp = ''  
+    for ex in expression: exp += ex
+    
+    num_data = list(map(str, exp.split('.')))
+    oper_com = list(permutations(set(oper_data), len(set(oper_data))))
+    
+    for i in range(len(num_data)):
+        og_expression.append(num_data[i])
+        if i != len(num_data) - 1:
+            og_expression.append(oper_data[i])
+    
+    
+    for com in range(len(oper_com)):
+        temp = list(og_expression)
         
-        
-        for start in range(cut_size, len(s), cut_size):
-            if prev_str == s[start : start + cut_size]:
-                overlap_count += 1
-            else:
-                if overlap_count > 1:
-                    result += str(overlap_count) + prev_str
+        for op in oper_com[com]:
+            stack = []
+            i = 0
+            while i < len(temp):
+                if temp[i] != op:
+                    stack.append(temp[i])
+                    i += 1
                 else:
-                    result += prev_str
-                    
-                prev_str = s[start : start + cut_size]
-                overlap_count = 1
-                
-        if overlap_count > 1:
-            result += str(overlap_count) + prev_str
-        else:
-            result += prev_str
+                    num_1 = int(stack.pop()) 
+                    num_2 = int(temp[i + 1])
+                    if temp[i] == '*':
+                        stack.append(num_1 * num_2)
+                    elif temp[i] == '+':
+                        stack.append(num_1 + num_2)
+                    elif temp[i] == '-':
+                        stack.append(num_1 - num_2)
+                    i += 2
+            temp = list(stack)
             
-        answer.append(len(result))
-        
-    return min(answer)
+        answer.append(abs(int(temp.pop())))
+    
+    return max(answer)
