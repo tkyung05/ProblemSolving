fd4d110367554cf9547ff0d60ab6aeb1b48be4fe
tkyung05 <tkyung6761@gmail.com>
2022-09-03 00:15:27 +0900

    PMS level 2 뉴스 클러스터링 : 2018 카카오 블라인드 채용 해시 문제 여전히 더러운 내 코드.

--- code.py
+++ code.py
@@ -1,45 +1,58 @@
-from collections import deque
+import math
 
-dy = [-1, 1, 0, 0]
-dx = [0, 0, -1, 1]
-
-def bfs(y, x, graph):
-    visited = [[0] * 5 for _ in range(5)]
-    visited[y][x] = 1
-    q = deque([(y, x)])
+def solution(str1, str2):
+    answer = 0
+    str1 = str1.upper()
+    str2 = str2.upper()
     
-    while q:
-        y, x = q.popleft()
+    
+    str1_hash = {}
+    str2_hash = {}
+    intersection_elements = {}
+    
+    for i in range(len(str1) - 1):
+        if 65 <= ord(str1[i]) <= 90 and 65 <= ord(str1[i + 1]) <= 90:
+            element = str1[i] + str1[i + 1]
+            if element not in str1_hash:
+                str1_hash[element] = 1
+            else:
+                str1_hash[element] += 1
 
-        if 1 < visited[y][x] < 4 and graph[y][x] == 'P':
-            return True
+    for i in range(len(str2) - 1):
+        if 65 <= ord(str2[i]) <= 90 and 65 <= ord(str2[i + 1]) <= 90:
+            element = str2[i] + str2[i + 1]
+            if element in str1_hash:
+                intersection_elements[element] = True
+            if element not in str2_hash:
+                str2_hash[element] = 1
+            else:
+                str2_hash[element] += 1
+
+                
+    
+    all_elements = set([k for k in str1_hash] + [k for k in str2_hash])
+    union, intersection = 0, 0
+
+    for e in all_elements:
+        max_num = 0
+        if e in str1_hash:
+            max_num = str1_hash[e]
+        if e in str2_hash:
+            max_num = max(max_num, str2_hash[e])
+        union += max_num
         
-        for i in range(4):
-            ny, nx = y + dy[i], x + dx[i]
-            if ny < 0 or ny > 4 or nx < 0 or nx > 4:
-                continue
-            if graph[ny][nx] == 'X':
-                continue
-            if visited[ny][nx] == 0:
-                visited[ny][nx] = visited[y][x] + 1
-                q.append((ny, nx))
+    for e in intersection_elements:
+        min_num = 0
+        if e in str1_hash:
+            min_num = str1_hash[e]
+        if e in str2_hash:
+            min_num = min(min_num, str2_hash[e])
+        intersection += min_num
     
-    return False
+    if intersection + union == 0:
+        intersection, union = 1, 1
     
-
-
-def solution(places):
-    answer = [0] * 5
-    
-    for i in range(5):
-        bug = 1
-        for y in range(5):
-            for x in range(5):
-                if places[i][y][x] == 'P':
-                    if bug == 1 and bfs(y, x, places[i]):
-                        bug = 0
-        answer[i] = bug
-        
+    answer = math.trunc((intersection / union) * 65536)
     return answer
 
 
