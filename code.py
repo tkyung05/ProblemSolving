8e41faca0376b21315dc3edc34203c51447c3e9b
tkyung05 <tkyung6761@gmail.com>
2022-09-02 18:02:30 +0900

    PMS level 2 거리두기 확인하기 : 2021 카카오 채용연계형 인턴십 간단한 bfs 문제.

--- code.py
+++ code.py
@@ -1,50 +1,48 @@
-from itertools import *
+from collections import deque
 
-def solution(exp):
-    answer = []
-    oper_data = []
-    expression = [ex for ex in exp]
-    og_expression = []
+dy = [-1, 1, 0, 0]
+dx = [0, 0, -1, 1]
+
+def bfs(y, x, graph):
+    visited = [[0] * 5 for _ in range(5)]
+    visited[y][x] = 1
+    q = deque([(y, x)])
     
-    for i in range(len(expression)):
-        if expression[i] == '*' or expression[i] == '-' or expression[i] == '+':
-            oper_data.append(expression[i])
-            expression[i] = '.'
-    
-    exp = ''  
-    for ex in expression: exp += ex
-    
-    num_data = list(map(str, exp.split('.')))
-    oper_com = list(permutations(set(oper_data), len(set(oper_data))))
-    
-    for i in range(len(num_data)):
-        og_expression.append(num_data[i])
-        if i != len(num_data) - 1:
-            og_expression.append(oper_data[i])
-    
-    
-    for com in range(len(oper_com)):
-        temp = list(og_expression)
+    while q:
+        y, x = q.popleft()
+
+        if 1 < visited[y][x] < 4 and graph[y][x] == 'P':
+            return True
         
-        for op in oper_com[com]:
-            stack = []
-            i = 0
-            while i < len(temp):
-                if temp[i] != op:
-                    stack.append(temp[i])
-                    i += 1
-                else:
-                    num_1 = int(stack.pop()) 
-                    num_2 = int(temp[i + 1])
-                    if temp[i] == '*':
-                        stack.append(num_1 * num_2)
-                    elif temp[i] == '+':
-                        stack.append(num_1 + num_2)
-                    elif temp[i] == '-':
-                        stack.append(num_1 - num_2)
-                    i += 2
-            temp = list(stack)
-            
-        answer.append(abs(int(temp.pop())))
+        for i in range(4):
+            ny, nx = y + dy[i], x + dx[i]
+            if ny < 0 or ny > 4 or nx < 0 or nx > 4:
+                continue
+            if graph[ny][nx] == 'X':
+                continue
+            if visited[ny][nx] == 0:
+                visited[ny][nx] = visited[y][x] + 1
+                q.append((ny, nx))
     
-    return max(answer)
+    return False
+    
+
+
+def solution(places):
+    answer = [0] * 5
+    
+    for i in range(5):
+        bug = 1
+        for y in range(5):
+            for x in range(5):
+                if places[i][y][x] == 'P':
+                    if bug == 1 and bfs(y, x, places[i]):
+                        bug = 0
+        answer[i] = bug
+        
+    return answer
+
+
+
+
+
