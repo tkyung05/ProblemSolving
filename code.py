4fd6bc6303b22f54f2e553f6db831ba622719190
tkyung05 <tkyung6761@gmail.com>
2022-09-06 16:28:02 +0900

    PMS level 1 [1차] 비밀지도 : 2018 카카오 블라인드 채용 문제. 부끄럽지만 오늘은 1렙만 풀어야지...

--- code.py
+++ code.py
@@ -1,22 +1,17 @@
-def solution(s):
-    answer = ''
+def solution(n, arr1, arr2):
+    answer = []
     
-    start_with = {'o' : ['ne'], 'z' : ['ero'], 't' : ['wo', 'hree'], 'f' : ['our', 'ive'],
-             's' : ['ix', 'even'], 'e' : ['ight'], 'n' : ['ine']}
-    
-    found_number = {'zero' : '0', 'one' : '1', 'two' : '2', 'three' : '3', 'four' : '4', 'five' : '5', 'six' : '6', 'seven' : '7', 'eight' : '8', 'nine' : '9'}
-    
-    idx = 0
-    
-    while idx < len(s):
-        if s[idx] not in start_with:
-            answer += s[idx]
-            idx += 1
-        else:
-            for word in start_with[s[idx]]:
-                if word[0] == s[idx + 1]:
-                    answer += found_number[s[idx] + word]  
-                    idx += len(word) + 1
-                    break
-            
-    return int(answer)
+    for i in range(n):
+        a, b = format(arr1[i], 'b'), format(arr2[i], 'b')
+        temp = str(int(a) + int(b))
+        temp = '0' * (n - len(temp)) + temp 
+        
+        row = []
+        for i in temp:
+            if i == '0': row.append(' ')
+            else: row.append('#')
+        
+        answer.append(''.join(row))
+        
+        
+    return answer
