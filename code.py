d5ff1823f24cca98ac98012faf8880187c6bd947
tkyung05 <tkyung6761@gmail.com>
2022-09-07 13:12:44 +0900

    PMS level 2 튜플 : 2019 카카오 개발자 겨울 인턴십 해시, 문자열, 탐색 문제. 오랜만에 level 2 했네.

--- code.py
+++ code.py
@@ -1,35 +1,32 @@
-def solution(dartResult):
+def solution(s):
     answer = []
+    s = s[1:-1]
     
-    option = {'*': 2, '#': -1}
-    increase = {'S': 1, 'D': 2, 'T': 3}
-    number = {str(i): i for i in range(10)}
-    
-    count = 0
+    transList = []
     idx = 0
-    
-    while idx < len(dartResult):
-        if dartResult[idx] in number:
-            num = int(dartResult[idx])
-            if dartResult[idx] == '1' and dartResult[idx + 1] == '0':
-                num = 10
-                idx += 1
-                
+    while idx < len(s):
+        if s[idx] == '{':
             idx += 1
-            answer.append(num ** increase[dartResult[idx]])
-            idx += 1
+            start = idx
+            while s[idx] != '}': idx += 1
+            end = idx    
             
-            if idx < len(dartResult) and dartResult[idx] in option:
-                if dartResult[idx] == '*':
-                    if count != 0:
-                        answer[count] = answer[count] * 2
-                        answer[count - 1] = answer[count - 1] * 2
-                    else:
-                        answer[count] = answer[count] * 2
-                else:
-                    answer[count] = answer[count] * -1
-                idx += 1
-            
-            count += 1
+            data = s[start:end].split(',')
+            transList.append((len(data), data))
+            idx += 2
+    transList.sort()
     
-    return sum(answer)
+    answerHash = {}
+    for l, data in transList:
+        for element in data:
+            if element not in answerHash:
+                answerHash[element] = True
+                answer.append(int(element))
+    
+    return answer
+
+
+
+
+
+
