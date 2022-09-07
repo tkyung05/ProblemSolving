b22e901964afbac486882c1be518bc244cc2fff9
tkyung05 <tkyung6761@gmail.com>
2022-09-07 10:19:00 +0900

    PMS level 1 [1차] 다트 게임 : 2018 카카오 블라인드 채용 기본 문자열, 해시, 탐색 문제.

--- code.py
+++ code.py
@@ -1,17 +1,35 @@
-def solution(n, arr1, arr2):
+def solution(dartResult):
     answer = []
     
-    for i in range(n):
-        a, b = format(arr1[i], 'b'), format(arr2[i], 'b')
-        temp = str(int(a) + int(b))
-        temp = '0' * (n - len(temp)) + temp 
-        
-        row = []
-        for i in temp:
-            if i == '0': row.append(' ')
-            else: row.append('#')
-        
-        answer.append(''.join(row))
-        
-        
-    return answer
+    option = {'*': 2, '#': -1}
+    increase = {'S': 1, 'D': 2, 'T': 3}
+    number = {str(i): i for i in range(10)}
+    
+    count = 0
+    idx = 0
+    
+    while idx < len(dartResult):
+        if dartResult[idx] in number:
+            num = int(dartResult[idx])
+            if dartResult[idx] == '1' and dartResult[idx + 1] == '0':
+                num = 10
+                idx += 1
+                
+            idx += 1
+            answer.append(num ** increase[dartResult[idx]])
+            idx += 1
+            
+            if idx < len(dartResult) and dartResult[idx] in option:
+                if dartResult[idx] == '*':
+                    if count != 0:
+                        answer[count] = answer[count] * 2
+                        answer[count - 1] = answer[count - 1] * 2
+                    else:
+                        answer[count] = answer[count] * 2
+                else:
+                    answer[count] = answer[count] * -1
+                idx += 1
+            
+            count += 1
+    
+    return sum(answer)
