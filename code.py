130cab20cfc375ab67f77962b493a9757660d6fd
tkyung05 <tkyung6761@gmail.com>
2022-09-09 23:16:14 +0900

    PMS level 2 이진 변환 반복하기 : 월드 코드 챌린지 시즌1 level 1 붙이기도 아까운 너무 쉬운 문제... 오늘은 날먹 데이.

--- code.py
+++ code.py
@@ -1,21 +1,12 @@
-def solution(n, words):
+def solution(s):
     answer = [0] * 2
-    overlapCheck = {}
 
-    if len(words[0]) == 1:
-        return [1, 1]
+    delZero, trans = 0, 0
+    while len(s) > 1:
+        countOne = sum(list(map(int, s)))
+        delZero += len(s) - countOne
+        s = format(countOne, 'b')
+        trans += 1
 
-    prev_word = words[0][-1]
-    overlapCheck[words[0]] = True
-
-    for i in range(1, len(words)):
-        if len(words[i]) > 1 and words[i][0] == prev_word and words[i] not in overlapCheck:
-            overlapCheck[words[i]] = True
-            prev_word = words[i][-1]
-        else:
-            answer[0] = (i % n) + 1
-            answer[1] = (i + 1) // n
-            answer[1] += 1 if (i + 1) % n != 0 else 0
-            break
-
+    answer[0], answer[1] = trans, delZero
     return answer
