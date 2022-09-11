65e441215a02b31dfef19e21e8174359f6cb7484
tkyung05 <tkyung6761@gmail.com>
2022-09-11 13:49:16 +0900

    PMS level 2 방문 길이 : 여름/겨울 코딩 쉬운 문자열, 구현 문제. 근데 12점이라 날먹 ㅋㅋㅋ.

--- code.py
+++ code.py
@@ -1,19 +1,24 @@
-def solution(skill, skill_trees):
+def solution(dirs):
     answer = 0
-    my_skill = {s: 1 for s in skill}
 
-    for skt in skill_trees:
-        cur = 0
-        impossible_skill = False
+    move = {'U': [-1, 0], 'D': [1, 0], 'R': [0, 1], 'L': [0, -1]}
+    player_y, player_x = 5, 5
+    overlap_way = {}
 
-        for s in skt:
-            if s in my_skill:
-                if skill[cur] == s:
-                    cur += 1
-                else:
-                    impossible_skill = True
-                    break
+    for cmd in dirs:
+        dy, dx = move[cmd]
+        ny, nx = player_y + dy, player_x + dx
 
-        answer += 0 if impossible_skill else 1
+        if ny < 0 or ny > 10 or nx < 0 or nx > 10:
+            continue
+
+        key = str(player_y) + str(player_x) + str(ny) + str(nx)
+
+        if key not in overlap_way:
+            overlap_way[key] = 1
+            overlap_way[str(ny) + str(nx) + str(player_y) + str(player_x)] = 1
+            answer += 1
+
+        player_y, player_x = ny, nx
 
     return answer
