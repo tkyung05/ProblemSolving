6058f2dcfccd8ec50fe1ad32e39eb76631dd89f9
tkyung05 <tkyung6761@gmail.com>
2022-09-13 08:33:06 +0900

    PMS level 2 n^2 배열 자르기 : 월코챌 시즌 2 오늘 천재 빙의했다 거의. 행렬 y, x 좌표 값을 몫과 나머지로 구해서 max.

--- code.py
+++ code.py
@@ -1,22 +1,7 @@
-def solution(numbers):
+def solution(n, left, right):
     answer = []
 
-    for num in numbers:
-        bin_num = format(num, 'b')
-        if len(bin_num) == sum(list(map(int, bin_num))):
-            bin_num = '0b10' + bin_num[1:]
-        else:
-            bin_list = list(bin_num)
-            for i in range(len(bin_list) - 1, -1, -1):
-                if bin_list[i] == '0':
-                    bin_list[i] = '1'
-                    for j in range(i + 1, len(bin_list)):
-                        if bin_list[j] == '1':
-                            bin_list[j] = '0'
-                            break
-                    break
-            bin_num = '0b' + ''.join(bin_list)
-
-        answer.append(int(bin_num, 2))
+    for idx in range(left, right + 1):
+        answer.append(max(idx // n, idx % n) + 1)
 
     return answer
