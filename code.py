d7e3dd8413648cad851896bd172863fd0fdadd99
tkyung05 <tkyung6761@gmail.com>
2022-09-13 15:07:12 +0900

    PMS level 2 구명보트 : 오랜만에 연습문제 그리디 문제 잘 못 읽었다가 내가 연습문제도 못 푸는 줄;;;ㄹㅇ ㅋㅋ

--- code.py
+++ code.py
@@ -1,7 +1,16 @@
-def solution(n, left, right):
-    answer = []
+from collections import deque
 
-    for idx in range(left, right + 1):
-        answer.append(max(idx // n, idx % n) + 1)
 
-    return answer
+def solution(people, limit):
+    answer = 0
+    people_weight = deque(sorted(people))
+
+    while len(people_weight) > 1:
+        if people_weight[0] + people_weight[-1] <= limit:
+            people_weight.popleft()
+            people_weight.pop()
+        else:
+            people_weight.pop()
+        answer += 1
+
+    return answer + len(people_weight)
