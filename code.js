b76a443c32316da1f3c2c29a77419e7aceff811a
tkyung05 <tkyung6761@gmail.com>
2022-09-15 20:08:08 +0900

    PMS level 2 JadenCase 문자열 만들기 : JS 문법익히는 겸 쉬운 문제.

--- code.js
+++ code.js
@@ -1,14 +1,6 @@
-const LIMIT = 60001
-const DIV_NUM = 1000000007
-
-function solution(n) {
-    let answer = Array.from({lenght: LIMIT}, () => 0);
-    answer[1] = 1;
-    answer[2] = 2;
-    
-    for (let i = 3; i < LIMIT; i++) {
-        answer[i] = (answer[i - 1] + answer[i - 2]) % DIV_NUM;
-    }
-    
-    return answer[n];
+function solution(s) {
+  const str = s.split(' ');
+  const answer = str.map(x => x.charAt(0).toUpperCase() + x.slice(1).toLowerCase());
+  
+  return answer.join(' ');
 }
